1a7e8822ec2176c63c221ddca0ff76ba3757ea14
rws <empty>
1990-05-18 11:44:52 +0000

    XChangeDeviceDontPropagateList ignores mode, hammers settings from others

--- xc/programs/Xserver/Xi/chgprop.c
+++ xc/programs/Xserver/Xi/chgprop.c
@@ -1,4 +1,4 @@
-/* $XConsortium: xchgprop.c,v 1.5 89/11/21 14:50:31 rws Exp $ */
+/* $XConsortium: xchgprop.c,v 1.6 89/12/02 15:20:36 rws Exp $ */
 
 /************************************************************
 Copyright (c) 1989 by Hewlett-Packard Company, Palo Alto, California, and the 
@@ -40,6 +40,7 @@ SOFTWARE.
 #include "XIproto.h"
 
 extern	int 	BadMode;
+extern	int 	BadClass;
 extern	int 	IReqCode;
 DeviceIntPtr	LookupDeviceIntRec();
 
@@ -79,9 +80,10 @@ SProcXChangeDeviceDontPropagateList(client)
 ProcXChangeDeviceDontPropagateList (client)
     register ClientPtr client;
     {
-    int		i;
-    WindowPtr	pWin;
-    struct tmask tmp[EMASKSIZE];
+    int			i, ret;
+    WindowPtr		pWin;
+    struct 		tmask tmp[EMASKSIZE];
+    OtherInputMasks	*others;
 
     REQUEST(xChangeDeviceDontPropagateListReq);
     REQUEST_AT_LEAST_SIZE(xChangeDeviceDontPropagateListReq);
@@ -115,8 +117,24 @@ ProcXChangeDeviceDontPropagateList (client)
 	stuff->count, tmp, NULL, X_ChangeDeviceDontPropagateList) != Success)
 	    return Success;
 
-    for (i=0; i<EMASKSIZE; i++)
-	DeviceEventSuppressForWindow(pWin, client, tmp[i].mask, i);
+    if (others = wOtherInputMasks(pWin))
+	for (i=0; i<EMASKSIZE; i++)
+	    {
+	    if (tmp[i].mask == 0)
+		continue;
+	    if (stuff->mode == AddToList)
+		tmp[i].mask |= others->dontPropagateMask[i];
+	    else
+		tmp[i].mask = (others->dontPropagateMask[i] & ~tmp[i].mask);
+
+	    if (DeviceEventSuppressForWindow (pWin,client,tmp[i].mask,i) != 
+			Success)
+		{
+		SendErrorToClient ( client, 
+			IReqCode, X_ChangeDeviceDontPropagateList, 0, BadClass);
+		return Success;
+		}
+	    }
 
     return Success;
     }
