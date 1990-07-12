84c3a4671a66b3f550ae3937959fb2a3cc849c96
swick <empty>
1990-07-12 13:29:45 +0000

    allow a specific warning to be disabled by giving it an empty message string.

--- xc/lib/Xt/Error.c
+++ xc/lib/Xt/Error.c
@@ -1,5 +1,5 @@
 #ifndef lint
-static char Xrcsid[] = "$XConsortium: Error.c,v 1.23 89/12/15 17:33:30 swick Exp $";
+static char Xrcsid[] = "$XConsortium: Error.c,v 1.24 90/03/19 12:58:53 swick Exp $";
 /* $oHeader: Error.c,v 1.6 88/08/31 17:46:14 asente Exp $ */
 #endif /* lint */
 
@@ -300,6 +300,7 @@ static void _XtDefaultError(message)
 static void _XtDefaultWarning(message)
     String message;
 {
+    if (message && *message)
        (void)fprintf(stderr, "%sWarning: %s\n", XTWARNING_PREFIX, message); 
     return;
 }
