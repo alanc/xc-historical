b166ac1dfd8d6eaa0cc4551d5feeccf3c74f7f69
rws <empty>
1990-07-26 18:31:38 +0000

    new implementation with fast hashing move XKeysymToString to a separate file, to cut down exec size

--- xc/lib/X11/StrKeysym.c
+++ xc/lib/X11/StrKeysym.c
@@ -1,8 +1,11 @@
-/* $XConsortium: XStrKeysym.c,v 11.4 89/12/11 19:10:34 rws Exp $ */
-/* Copyright 1985, 1987, Massachusetts Institute of Technology */
+/* $XConsortium: XStrKeysym.c,v 11.5 90/06/15 11:24:25 rws Exp $ */
+/* Copyright 1985, 1987, 1990 Massachusetts Institute of Technology */
 
 #include "Xlibint.h"
 #include <X11/Xresource.h>
+#include <X11/keysymdef.h>
+
+extern XrmQuark _XrmInternalStringToQuark();
 
 #ifdef __STDC__
 #define Const const
@@ -10,25 +13,30 @@
 #define Const /**/
 #endif
 
-static Const struct ks_info {
-    Const char	*ks_name;
-    KeySym	ks_val;
-} keySymInfo[] = {
-#include	"ks_names.h"
-};
+typedef unsigned long Signature;
+
+#define NEEDKTABLE
+#include "ks_tables.h"
 
 #ifndef KEYSYMDB
 #define KEYSYMDB "/usr/lib/X11/XKeysymDB"
 #endif
 
-static Bool initialized = False;
+static Bool initialized;
 static XrmDatabase keysymdb;
+static XrmQuark Qkeysym[2];
 
+XrmDatabase
 _XInitKeysymDB()
 {
-    XrmInitialize();
-    keysymdb = XrmGetFileDatabase(KEYSYMDB);
-    initialized = True;
+    if (!initialized)
+    {
+	XrmInitialize();
+	keysymdb = XrmGetFileDatabase(KEYSYMDB);
+	Qkeysym[0] = XrmStringToQuark("Keysym");
+	initialized = True;
+    }
+    return keysymdb;
 }
 
 #if NeedFunctionPrototypes
@@ -39,71 +47,67 @@ KeySym XStringToKeysym(s)
     char *s;
 #endif
 {
-    int i;
+    register int i, n;
+    int h;
+    register Signature sig = 0;
+    register Const char *p = s;
+    register int c;
+    register int idx;
+    Const unsigned char *entry;
+    unsigned char sig1, sig2;
+    KeySym val;
 
-    /*
-     *	Yes,  yes,  yes.  I know this is a linear search,  and we should
-     *	do better,  but I'm in a hurry right now.
-     */
-
-    for (i = 0; i < ((sizeof keySymInfo)/(sizeof keySymInfo[0])); i++) {
-	if (strcmp(s, keySymInfo[i].ks_name) == 0)
-	    return (keySymInfo[i].ks_val);
+    while (c = *p++)
+	sig = (sig << 1) + c;
+    i = sig % KTABLESIZE;
+    h = i + 1;
+    sig1 = (sig >> 8) & 0xff;
+    sig2 = sig & 0xff;
+    n = KMAXHASH;
+    while (idx = hashString[i])
+    {
+	entry = &__XkeyTable[idx];
+	if ((entry[0] == sig1) && (entry[1] == sig2) &&
+	    !strcmp(s, (char *)entry + 4))
+	{
+	    val = (entry[2] << 8) | entry[3];
+	    if (!val)
+		val = XK_VoidSymbol;
+	    return val;
+	}
+	if (!--n)
+	    break;
+	i += h;
+	if (i >= KTABLESIZE)
+	    i -= KTABLESIZE;
     }
+
     if (!initialized)
-	_XInitKeysymDB();
+	(void)_XInitKeysymDB();
     if (keysymdb)
     {
-	XrmString type_str;
 	XrmValue result;
+	XrmRepresentation from_type;
 	char c;
 	KeySym val;
+	XrmQuark names[2];
 
-	XrmGetResource(keysymdb, s, "Keysym", &type_str, &result);
+	names[0] = _XrmInternalStringToQuark(s, strlen(s), sig);
+	names[1] = NULLQUARK;
+	(void)XrmQGetResource(keysymdb, names, Qkeysym, &from_type, &result);
 	if (result.addr && (result.size > 1))
 	{
 	    val = 0;
 	    for (i = 0; i < result.size - 1; i++)
 	    {
 		c = ((char *)result.addr)[i];
-		if ('0' <= c && c <= '9') val = val*16+c-'0';
-		else if ('a' <= c && c <= 'z') val = val*16+c-'a'+10;
-		else if ('A' <= c && c <= 'Z') val = val*16+c-'A'+10;
+		if ('0' <= c && c <= '9') val = (val<<4)+c-'0';
+		else if ('a' <= c && c <= 'z') val = (val<<4)+c-'a'+10;
+		else if ('A' <= c && c <= 'Z') val = (val<<4)+c-'A'+10;
 		else return NoSymbol;
 	    }
 	    return val;
 	}
     }
     return (NoSymbol);
-}
-
-extern char *_XrmGetResourceName();
-
-char *XKeysymToString(ks)
-    KeySym ks;
-{
-    int i;
-
-    /*
-     *	Yes,  yes,  yes.  I know this is a linear search,  and we should
-     *	do better,  but I'm in a hurry right now.
-     */
-
-    for (i = 0; i < ((sizeof keySymInfo)/(sizeof keySymInfo[0])); i++) {
-	if (ks == keySymInfo[i].ks_val)
-	    return ((char *)(keySymInfo[i].ks_name));
-    }
-    if (!initialized)
-	_XInitKeysymDB();
-    if (keysymdb)
-    {
-	char buf[8];
-	XrmValue resval;
-
-	sprintf(buf, "%lX", ks);
-	resval.addr = (caddr_t)buf;
-	resval.size = strlen(buf) + 1;
-	return _XrmGetResourceName(keysymdb, "String", &resval);
-    }
-    return ((char *) NULL);
 }
