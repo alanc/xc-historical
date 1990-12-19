6de01647f46c538f4e6d4e1d4c64d31517ac7664
converse <empty>
1990-12-19 19:08:59 +0000

    replace #include copyright.h with a real copyright.

--- xc/lib/Xmu/StrToWidg.c
+++ xc/lib/Xmu/StrToWidg.c
@@ -1,6 +1,24 @@
-/* static char Xrcsid[] = "$XConsortium: StrToWidg.c,v 1.3 89/09/22 12:11:59 keith Exp $"; */
+/* $XConsortium: StrToWidg.c,v 1.4 90/07/15 16:19:03 rws Exp $ */
 
-/* Copyright 1988 Massachusetts Institute of Technology, Cambridge, Massachusetts.
+/* Copyright 1988 Massachusetts Institute of Technology
+ *
+ * Permission to use, copy, modify, distribute, and sell this software and its
+ * documentation for any purpose is hereby granted without fee, provided that
+ * the above copyright notice appear in all copies and that both that
+ * copyright notice and this permission notice appear in supporting
+ * documentation, and that the name of M.I.T. not be used in advertising or
+ * publicity pertaining to distribution of the software without specific,
+ * written prior permission.  M.I.T. makes no representations about the
+ * suitability of this software for any purpose.  It is provided "as is"
+ * without express or implied warranty.
+ *
+ * M.I.T. DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING ALL
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO EVENT SHALL M.I.T.
+ * BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
+ * OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
  *
  * XmuCvtStringToWidget
  *
@@ -12,8 +30,6 @@
  * or popup) of the parent.  If none match, compares string to classname
  * & returns first match.  Case is significant.
  */
-
-#include <X11/copyright.h>
 
 #include <X11/IntrinsicP.h>
 #include <X11/ObjectP.h>
