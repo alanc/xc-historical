02180acdbe2bae203664d804c3ab4310f5b70847
gildea <empty>
1990-12-21 11:41:46 +0000

    separate out initialization code

--- xc/unsupported/programs/xload/get_load.c
+++ xc/unsupported/programs/xload/get_load.c
@@ -1,4 +1,8 @@
 /*
+ * get_load - get system load
+ *
+ * $XConsortium$
+ *
  * Copyright 1989 Massachusetts Institute of Technology
  *
  * Permission to use, copy, modify, distribute, and sell this software and its
@@ -19,6 +23,9 @@
  * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  *
  * Authors:  Many and varied...
+ *
+ * Call InitLoadPoint() to initialize.
+ * GetLoadPoint() is a callback for the StripChart widget.
  */
 
 #include <stdio.h>
@@ -28,30 +35,6 @@
 #ifdef att
 #define LOADSTUB
 #endif
-
-#ifdef apollo
-#include <apollo/base.h>
-#include <apollo/time.h>
-typedef struct {
-	short		state;		/* ready, waiting, etc. */
-	pinteger	usr;		/* user sr */
-	linteger	upc;		/* user pc */
-	linteger	usp;		/* user stack pointer */
-	linteger	usb;		/* user sb ptr (A6) */
-	time_$clock_t	cpu_total;	/* cumulative cpu used by process */
-	unsigned short	priority;	/* process priority */
-    } proc1_$info_t;
-
-void proc1_$get_cput(
-	time_$clock_t	*cput
-);
-
-void proc1_$get_info(
-	short		&pid,
-	proc1_$info_t	*info,
-	status_$t	*sts
-);
-#endif /* apollo */
 
 #ifndef macII
 #ifndef apollo
@@ -106,16 +89,54 @@ struct lavnum {
 extern long lseek();
 extern void exit();
 
-#if apollo
+static xload_error();
+
+
+#ifdef apollo
+#include <apollo/base.h>
+#include <apollo/time.h>
+typedef struct {
+	short		state;		/* ready, waiting, etc. */
+	pinteger	usr;		/* user sr */
+	linteger	upc;		/* user pc */
+	linteger	usp;		/* user stack pointer */
+	linteger	usb;		/* user sb ptr (A6) */
+	time_$clock_t	cpu_total;	/* cumulative cpu used by process */
+	unsigned short	priority;	/* process priority */
+    } proc1_$info_t;
+
+void proc1_$get_cput(
+	time_$clock_t	*cput
+);
+
+void proc1_$get_info(
+	short		&pid,
+	proc1_$info_t	*info,
+	status_$t	*sts
+);
+
+static int     lastNullCpu;
+static int     lastClock;
+
+void InitLoadPoint()				/* Apollo version */
+{
+     time_$clock_t  timeNow;
+     proc1_$info_t  info;
+     status_$t      st;
+
+     proc1_$get_info( (short) 2, &info, &st );
+     time_$clock( &timeNow );
+
+     lastClock = timeNow.low32;
+     lastNullCpu = info.cpu_total.low32;
+}
+
 /* ARGSUSED */
-void GetLoadPoint( w, closure, call_data )
+void GetLoadPoint( w, closure, call_data ) 	/* Apollo version */
      Widget	w;		/* unused */
      caddr_t	closure;	/* unused */
      caddr_t	call_data;	/* pointer to (double) return value */
 {
-     static Bool    firstTime = TRUE;
-     static int     lastNullCpu;
-     static int     lastClock;
      time_$clock_t  timeNow;
      double         temp;
      proc1_$info_t  info;
@@ -124,64 +145,63 @@ void GetLoadPoint( w, closure, call_data )
      proc1_$get_info( (short) 2, &info, &st );
      time_$clock( &timeNow );
 
-     if (firstTime)
-     {
-     	*(double *)call_data = 1.0;
-         firstTime = FALSE;
-     }
-     else {
-         temp = info.cpu_total.low32 - lastNullCpu;
-     	*(double *)call_data = 1.0 - temp / (timeNow.low32 - lastClock);
-     }
+     temp = info.cpu_total.low32 - lastNullCpu;
+     *(double *)call_data = 1.0 - temp / (timeNow.low32 - lastClock);
 
      lastClock = timeNow.low32;
      lastNullCpu = info.cpu_total.low32;
 }
-#else
+#else /* not apollo */
 #ifdef KVM_ROUTINES
 /*
  *	Sun 386i Code - abstracted to see the wood for the trees
  */
 
+static struct nlist nl[2];
+static kvm_t *kd;
+
+void
+InitLoadPoint()					/* Sun 386i version */
+{
+    kd = kvm_open("/vmunix", NULL, NULL, O_RDONLY, "Load Widget");
+    if (kd == (kvm_t *)0) {
+	xload_error("cannot get access to kernel address space");
+    }
+	
+    nl[0].n_name = "avenrun";
+    nl[1].n_name = NULL;
+	
+    if (kvm_nlist(kd, nl) != 0) {
+	xload_error("cannot get name list");
+    }
+    
+    if (nl[0].n_value == 0) {
+	xload_error("Cannot find address for avenrun in the kernel\n");
+    }
+}
+
 /* ARGSUSED */
 void 
-GetLoadPoint( w, closure, call_data )
+GetLoadPoint( w, closure, call_data ) 		/* Sun 386i version */
 Widget	w;		/* unused */
 XtPointer closure;	/* unused */
 XtPointer call_data;	/* pointer to (double) return value */
 {
     double *loadavg = (double *)call_data;
     long	temp;
-    static int init = 0;
-    static struct nlist nl[2];
-    static kvm_t *kd;
-    
-    if (!init) {
-	kd = kvm_open("/vmunix", NULL, NULL, O_RDONLY, "Load Widget");
-	if (kd == (kvm_t *)0) {
-	    xload_error("cannot get access to kernel address space");
-	}
-	
-	nl[0].n_name = "avenrun";
-	nl[1].n_name = NULL;
-	
-	if (kvm_nlist(kd, nl) != 0) {
-	    xload_error("cannot get name list");
-	}
-	init = 1;
-    }
-    
-    if (nl[0].n_value == 0) {
-	xload_error("Cannot find address for avenrun in the kernel\n");
-    }
+
     if (kvm_read(kd, nl[0].n_value, (char *)&temp, sizeof (temp)) != 
 	sizeof (temp)) {
 	xload_error("Kernel read error");
     }
     *loadavg = (double)temp/FSCALE;
 }
-#else /* KVM_ROUTINES */
+#else /* not KVM_ROUTINES */
 #ifdef LOADSTUB
+
+void InitLoadPoint()
+{
+}
 
 /* ARGSUSED */
 void GetLoadPoint( w, closure, call_data )
@@ -192,7 +212,7 @@ void GetLoadPoint( w, closure, call_data )
 	*(double *)call_data = 1.0;
 }
 
-#else /* LOADSTUB */
+#else /* not LOADSTUB */
 
 #ifndef KMEM_FILE
 #define KMEM_FILE "/dev/kmem"
@@ -269,9 +289,6 @@ void GetLoadPoint( w, closure, call_data )
 #        endif /* SYSTYPE_SYSV */
 #    endif /* mips */
 
-#    ifdef sequent
-#        define KERNEL_FILE "/dynix"
-#    endif /* sequent */
 #endif /* KERNEL_LOAD_VARIABLE */
 
 /*
@@ -288,7 +305,7 @@ void GetLoadPoint( w, closure, call_data )
 #endif /* KERNEL_LOAD_VARIABLE */
 
 #ifdef macII
-struct var v;
+static struct var v;
 static int pad[2];	/* This padding is needed if xload compiled on */
 			/* a/ux 1.1 is executed on a/ux 1.0, because */
 			/* the var structure had too much padding in 1.0, */
@@ -304,6 +321,60 @@ static struct nlist namelist[] = {	    /* namelist for vmunix grubbing */
 };
 #endif /* macII */
 
+static kmem;
+static long loadavg_seek;
+
+InitLoadPoint()
+{
+#ifdef macII
+    extern nlist();
+
+    int i;
+
+    strcpy(nl[0].n_name, "avenrun");
+    nl[1].n_name[0] = '\0';
+
+    kmem = open(KMEM_FILE, O_RDONLY);
+    if (kmem < 0) {
+	xload_error("cannot open", KMEM_FILE);
+    }
+
+    uvar(&v);
+
+    if (nlist( KERNEL_FILE, nl) != 0) {
+	xload_error("cannot get name list from", KERNEL_FILE);
+    }
+    for (i = 0; i < 2; i++) {
+	nl[i].n_value = (int)nl[i].n_value - v.v_kvoffset;
+    }
+#else /* not macII */
+    extern void nlist();
+	
+    nlist( KERNEL_FILE, namelist);
+    /*
+     * Some systems appear to set only one of these to Zero if the entry could
+     * not be found, I hope no_one returns Zero as a good value, or bad things
+     * will happen to you.  (I have a hard time believing the value will
+     * ever really be zero anyway).   CDP 5/17/89.
+     */
+    if (namelist[LOADAV].n_type == 0 ||
+	namelist[LOADAV].n_value == 0) {
+	xload_error("cannot get name list from", KERNEL_FILE);
+	exit(-1);
+    }
+    loadavg_seek = namelist[LOADAV].n_value;
+#if defined(mips) && defined(SYSTYPE_SYSV)
+    loadavg_seek &= 0x7fffffff;
+#endif /* mips && SYSTYPE_SYSV */
+#if (defined(CRAY) && defined(SYSINFO))
+    loadavg_seek += ((char *) (((struct sysinfo *)NULL)->avenrun)) -
+	((char *) NULL);
+#endif /* CRAY && SYSINFO */
+  
+    kmem = open(KMEM_FILE, O_RDONLY);
+    if (kmem < 0) xload_error("cannot open", KMEM_FILE);
+#endif /* macII else */
+}
 
 /* ARGSUSED */
 void GetLoadPoint( w, closure, call_data )
@@ -312,67 +383,13 @@ void GetLoadPoint( w, closure, call_data )
      caddr_t	call_data;	/* pointer to (double) return value */
 {
   	double *loadavg = (double *)call_data;
-	static int init = 0;
-	static kmem;
-	static long loadavg_seek;
-	static xload_error();
+
 #ifdef macII
-        extern nlist();
-
-        if(!init)   {
-            int i;
-
-            strcpy(nl[0].n_name, "avenrun");
-            nl[1].n_name[0] = '\0';
-
-            kmem = open(KMEM_FILE, O_RDONLY);
-            if (kmem < 0) {
-	        xload_error("cannot open", KMEM_FILE);
-            }
-
-            uvar(&v);
-
-            if (nlist( KERNEL_FILE, nl) != 0) {
-		xload_error("cannot get name list from", KERNEL_FILE);
-            }
-            for (i = 0; i < 2; i++) {
-                nl[i].n_value = (int)nl[i].n_value - v.v_kvoffset;
-            }
-            init = 1;
-        }
-#else /* not macII */
-	extern void nlist();
-	
-	if(!init)   {
-	    nlist( KERNEL_FILE, namelist);
-/*
- * Some systems appear to set only one of these to Zero if the entry could
- * not be found, I hope no_one returns Zero as a good value, or bad things
- * will happen to you.  (I have a hard time believing the value will
- * ever really be zero anyway).   CDP 5/17/89.
- */
-	    if (namelist[LOADAV].n_type == 0 ||
-		namelist[LOADAV].n_value == 0) {
-		xload_error("cannot get name list from", KERNEL_FILE);
-		exit(-1);
-	    }
-	    loadavg_seek = namelist[LOADAV].n_value;
-#if defined(mips) && defined(SYSTYPE_SYSV)
-	    loadavg_seek &= 0x7fffffff;
-#endif /* mips && SYSTYPE_SYSV */
-#if (defined(CRAY) && defined(SYSINFO))
- 	    loadavg_seek += ((char *) (((struct sysinfo *)NULL)->avenrun)) -
-	                    ((char *) NULL);
-#endif /* SYSINFO */
-  
-	    kmem = open(KMEM_FILE, O_RDONLY);
-	    if (kmem < 0) xload_error("cannot open", KMEM_FILE);
-	    init = 1;
-	}
-	
-
+	lseek(kmem, (long)nl[X_AVENRUN].n_value, 0);
+#else
 	(void) lseek(kmem, loadavg_seek, 0);
-#endif /* macII */
+#endif
+
 #if defined(sun) || defined (UTEK) || defined(sequent) || defined(alliant)
 	{
 		long temp;
@@ -382,7 +399,6 @@ void GetLoadPoint( w, closure, call_data )
 #else /* else not sun */
 # ifdef macII
         {
-                lseek(kmem, (long)nl[X_AVENRUN].n_value, 0);
                 read(kmem, vec, 3*sizeof(struct lavnum));
                 *loadavg = fxtod(0);
         }
@@ -395,14 +411,13 @@ void GetLoadPoint( w, closure, call_data )
 	}
 #  else /* not mips */
 	(void) read(kmem, (char *)loadavg, sizeof(double));
-#  endif /* mips */
-# endif /* macII */
-#endif /* sun */
+#  endif /* mips else */
+# endif /* macII else */
+#endif /* sun else */
 	return;
 }
-#endif /* LOADSTUB */
-#endif /* KVM_ROUTINES */
-#endif /* apollo */
+#endif /* LOADSTUB else */
+#endif /* KVM_ROUTINES else */
 
 static xload_error(str1, str2)
 char *str1, *str2;
@@ -410,4 +425,6 @@ char *str1, *str2;
     (void) fprintf(stderr,"xload: %s %s\n", str1, str2);
     exit(-1);
 }
+
+#endif /* apollo else */
 
