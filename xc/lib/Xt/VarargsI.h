559a4bcf2b370a544da80837f07c711a9c1f1628
rws <empty>
1990-12-27 14:08:16 +0000

    move more varargs stuff here from Xtos.h try to clean things up a bit hack to make apollo work

--- xc/lib/Xt/VarargsI.h
+++ xc/lib/Xt/VarargsI.h
@@ -1,4 +1,4 @@
-/* $XConsortium: VarargsI.h,v 1.9 90/12/11 12:01:53 rws Exp $ */
+/* $XConsortium: VarargsI.h,v 1.10 90/12/12 14:54:01 rws Exp $ */
 /*
 
 Copyright 1985, 1986, 1987, 1988, 1989 by the
@@ -21,12 +21,48 @@ without express or implied warranty.
 #ifndef _VarargsI_h_ 
 #define _VarargsI_h_ 
  
+#ifdef __HIGHC__
+#ifndef _STDARG_H
+#define _STDARG_H
+
+typedef char *va_list;
+
+/* Amount of space required in an argument list for an arg of type TYPE.
+   TYPE may alternatively be an expression whose type is used.  */
+
+#define __va_rounded_size(TYPE)  \
+  (((sizeof (TYPE) + sizeof (int) - 1) / sizeof (int)) * sizeof (int))
+
+#define va_start(AP, LASTARG)                                           \
+ (AP = ((char *) &(LASTARG) + __va_rounded_size (LASTARG)))
+
+#define va_end(AP)
+
+#define va_arg(AP, TYPE)                                                \
+ (AP += __va_rounded_size (TYPE),                                       \
+  *((TYPE *) (AP - __va_rounded_size (TYPE))))
+
+#endif /* _STDARG_H */
+#endif /* __HIGHC__ */
+
 #if __STDC__
-# ifndef MISSING_STDARG_H
-#  include <stdarg.h>
+# ifndef NOSTDHDRS
+#  define _XSTDVAR
+# else
+# ifdef __GNUC__
+#  define _XSTDVAR
 # endif
+# endif
+#endif
+
+#ifdef _XSTDVAR
+# include <stdarg.h>
 # define Va_start(a,b) va_start(a,b)
 #else
+# if NeedVarargsPrototypes
+#  undef NeedVarargsPrototypes
+#  define NeedVarargsPrototypes 0
+# endif
 # include <varargs.h>
 # define Va_start(a,b) va_start(a)
 #endif
