572880ff7fc5d4cede475c03771b33a9cc5ed842
rws <empty>
1990-12-29 12:17:51 +0000

    add keycache to cache XtTranslateKeycode results fix VMSism for "const" so structures that can be remain const

--- xc/lib/Xt/TMstate.c
+++ xc/lib/Xt/TMstate.c
@@ -1,4 +1,4 @@
-/* $XConsortium: TMstate.c,v 1.124 90/12/28 17:20:57 gildea Exp $ */
+/* $XConsortium: TMstate.c,v 1.125 90/12/28 17:40:08 gildea Exp $ */
 
 /*LINTLIBRARY*/
 
@@ -38,11 +38,17 @@ SOFTWARE.
 #include <stdio.h>
 #include "IntrinsicI.h"
 
-/* VMS linker doesn't believe relocatable addrs can be const */
 #if __STDC__ && !defined(VMS)
 #define Const const
+/* VMS linker doesn't believe relocatable addrs can be const */
+#ifdef VMS
+#define RConst /**/
+#else
+#define RConst const
+#endif
 #else
 #define Const /**/
+#define RConst /**/
 #endif
 
 static String XtNtranslationError = "translationError";
@@ -469,17 +475,82 @@ Boolean _XtRegularMatch(event,eventSeq)
 
 }
 
+#define FLUSHKEYCACHE(ctx) bzero((char *)&ctx->keycache, sizeof(TMKeyCache))
 
-static TMContext AllocTMContext(dpy)
-    Display *dpy;
+void _XtAllocTMContext(pd)
+    XtPerDisplay pd;
 {
-    XtPerDisplay pd = _XtGetPerDisplay(dpy);
-    TMContext ctx =
-	(TMContext)_XtHeapAlloc( &pd->heap, (unsigned)sizeof(TMContextRec) );
+    TMContext ctx;
+
+    ctx = (TMContext)_XtHeapAlloc( &pd->heap, (unsigned)sizeof(TMContextRec) );
+    ctx->event = NULL;
+    ctx->serial = 0;
+    ctx->keysym = NoSymbol;
+    ctx->modifiers = 0;
+    FLUSHKEYCACHE(ctx);
     pd->tm_context = ctx;
-    return ctx;
 }
 
+/*
+ * The following array reorders the modifier bits so that the most common ones
+ * (used by a translator) are in the top-most bits with respect to the size of
+ * the keycache.  The array currently just reverses the bits as a good guess.
+ * This might be more trouble than it is worth, but it seems to help.
+ */
+
+#define FM(i) i >> (8 - TMKEYCACHELOG2)
+static Const unsigned char modmix[256] = {
+FM(0x00), FM(0x80), FM(0x40), FM(0xc0), FM(0x20), FM(0xa0), FM(0x60), FM(0xe0),
+FM(0x10), FM(0x90), FM(0x50), FM(0xd0), FM(0x30), FM(0xb0), FM(0x70), FM(0xf0),
+FM(0x08), FM(0x88), FM(0x48), FM(0xc8), FM(0x28), FM(0xa8), FM(0x68), FM(0xe8),
+FM(0x18), FM(0x98), FM(0x58), FM(0xd8), FM(0x38), FM(0xb8), FM(0x78), FM(0xf8),
+FM(0x04), FM(0x84), FM(0x44), FM(0xc4), FM(0x24), FM(0xa4), FM(0x64), FM(0xe4),
+FM(0x14), FM(0x94), FM(0x54), FM(0xd4), FM(0x34), FM(0xb4), FM(0x74), FM(0xf4),
+FM(0x0c), FM(0x8c), FM(0x4c), FM(0xcc), FM(0x2c), FM(0xac), FM(0x6c), FM(0xec),
+FM(0x1c), FM(0x9c), FM(0x5c), FM(0xdc), FM(0x3c), FM(0xbc), FM(0x7c), FM(0xfc),
+FM(0x02), FM(0x82), FM(0x42), FM(0xc2), FM(0x22), FM(0xa2), FM(0x62), FM(0xe2),
+FM(0x12), FM(0x92), FM(0x52), FM(0xd2), FM(0x32), FM(0xb2), FM(0x72), FM(0xf2),
+FM(0x0a), FM(0x8a), FM(0x4a), FM(0xca), FM(0x2a), FM(0xaa), FM(0x6a), FM(0xea),
+FM(0x1a), FM(0x9a), FM(0x5a), FM(0xda), FM(0x3a), FM(0xba), FM(0x7a), FM(0xfa),
+FM(0x06), FM(0x86), FM(0x46), FM(0xc6), FM(0x26), FM(0xa6), FM(0x66), FM(0xe6),
+FM(0x16), FM(0x96), FM(0x56), FM(0xd6), FM(0x36), FM(0xb6), FM(0x76), FM(0xf6),
+FM(0x0e), FM(0x8e), FM(0x4e), FM(0xce), FM(0x2e), FM(0xae), FM(0x6e), FM(0xee),
+FM(0x1e), FM(0x9e), FM(0x5e), FM(0xde), FM(0x3e), FM(0xbe), FM(0x7e), FM(0xfe),
+FM(0x01), FM(0x81), FM(0x41), FM(0xc1), FM(0x21), FM(0xa1), FM(0x61), FM(0xe1),
+FM(0x11), FM(0x91), FM(0x51), FM(0xd1), FM(0x31), FM(0xb1), FM(0x71), FM(0xf1),
+FM(0x09), FM(0x89), FM(0x49), FM(0xc9), FM(0x29), FM(0xa9), FM(0x69), FM(0xe9),
+FM(0x19), FM(0x99), FM(0x59), FM(0xd9), FM(0x39), FM(0xb9), FM(0x79), FM(0xf9),
+FM(0x05), FM(0x85), FM(0x45), FM(0xc5), FM(0x25), FM(0xa5), FM(0x65), FM(0xe5),
+FM(0x15), FM(0x95), FM(0x55), FM(0xd5), FM(0x35), FM(0xb5), FM(0x75), FM(0xf5),
+FM(0x0d), FM(0x8d), FM(0x4d), FM(0xcd), FM(0x2d), FM(0xad), FM(0x6d), FM(0xed),
+FM(0x1d), FM(0x9d), FM(0x5d), FM(0xdd), FM(0x3d), FM(0xbd), FM(0x7d), FM(0xfd),
+FM(0x03), FM(0x83), FM(0x43), FM(0xc3), FM(0x23), FM(0xa3), FM(0x63), FM(0xe3),
+FM(0x13), FM(0x93), FM(0x53), FM(0xd3), FM(0x33), FM(0xb3), FM(0x73), FM(0xf3),
+FM(0x0b), FM(0x8b), FM(0x4b), FM(0xcb), FM(0x2b), FM(0xab), FM(0x6b), FM(0xeb),
+FM(0x1b), FM(0x9b), FM(0x5b), FM(0xdb), FM(0x3b), FM(0xbb), FM(0x7b), FM(0xfb),
+FM(0x07), FM(0x87), FM(0x47), FM(0xc7), FM(0x27), FM(0xa7), FM(0x67), FM(0xe7),
+FM(0x17), FM(0x97), FM(0x57), FM(0xd7), FM(0x37), FM(0xb7), FM(0x77), FM(0xf7),
+FM(0x0f), FM(0x8f), FM(0x4f), FM(0xcf), FM(0x2f), FM(0xaf), FM(0x6f), FM(0xef),
+FM(0x1f), FM(0x9f), FM(0x5f), FM(0xdf), FM(0x3f), FM(0xbf), FM(0x7f), FM(0xff)
+};
+#undef FM
+
+#define TRANSLATE(ctx,pd,dpy,key,mod,mod_ret,sym_ret) \
+{ \
+    int _i_ = (((key) - (pd)->min_keycode + modmix[(mod) & 0xff]) & \
+	       (TMKEYCACHESIZE-1)); \
+    if ((ctx)->keycache.keycode[_i_] == (key) && \
+	(ctx)->keycache.modifiers[_i_] == (mod)) { \
+	mod_ret = (ctx)->keycache.modifiers_return; \
+	sym_ret = (ctx)->keycache.keysym[_i_]; \
+    } else { \
+	XtTranslateKeycode(dpy, key, mod, &mod_ret, &sym_ret); \
+	(ctx)->keycache.keycode[_i_] = key; \
+	(ctx)->keycache.modifiers[_i_] = (unsigned short)(mod); \
+	(ctx)->keycache.keysym[_i_] = sym_ret; \
+	(ctx)->keycache.modifiers_return = mod_ret; \
+    } \
+}
 
 Boolean _XtMatchUsingDontCareMods(event,eventSeq)
     Event *event;
@@ -493,6 +564,8 @@ Boolean _XtMatchUsingDontCareMods(event,eventSeq)
     Modifiers computedMask = 0;
     Boolean resolved = TRUE;
     Display *dpy = eventSeq->xev->xany.display;
+    XtPerDisplay pd;
+    TMContext tm_context;
 
     if (event->lateModifiers != NULL)
         resolved = ComputeLateBindings(event,eventSeq,&computed,&computedMask);
@@ -503,11 +576,11 @@ Boolean _XtMatchUsingDontCareMods(event,eventSeq)
     if ( (computed & computedMask) ==
         (eventSeq->event.modifiers & computedMask) ) {
 	Modifiers least_mod;
-        XtTranslateKeycode(dpy, (KeyCode)eventSeq->event.eventCode,
-			   (unsigned)0, &modifiers_return, &keysym_return);
+	pd = _XtGetPerDisplay(dpy);
+	tm_context = pd->tm_context;
+        TRANSLATE(tm_context, pd, dpy, (KeyCode)eventSeq->event.eventCode,
+		  (unsigned)0, modifiers_return, keysym_return);
         if ((keysym_return & event->eventCodeMask)  == event->eventCode ) {
-	    TMContext tm_context = _XtGetPerDisplay(dpy)->tm_context;
-	    if (tm_context == NULL) tm_context = AllocTMContext(dpy);
 	    tm_context->event = eventSeq->xev;
 	    tm_context->serial = eventSeq->xev->xany.serial;
 	    tm_context->keysym = keysym_return;
@@ -520,12 +593,11 @@ Boolean _XtMatchUsingDontCareMods(event,eventSeq)
         for (i = modifiers_return; i >= least_mod; i--)
 	    /* all useful combinations of 8 modifier bits */
             if (useful_mods & i) {
-		 XtTranslateKeycode(dpy, (KeyCode)eventSeq->event.eventCode,
-			      (Modifiers)i, &modifiers_return,&keysym_return);
+		 TRANSLATE(tm_context, pd, dpy,
+			   (KeyCode)eventSeq->event.eventCode,
+			   (Modifiers)i, modifiers_return, keysym_return);
                  if (keysym_return  ==
                      (event->eventCode & event->eventCodeMask)) {
-		     TMContext tm_context = _XtGetPerDisplay(dpy)->tm_context;
-		     if (tm_context == NULL) tm_context = AllocTMContext(dpy);
 		     tm_context->event = eventSeq->xev;
 		     tm_context->serial = eventSeq->xev->xany.serial;
 		     tm_context->keysym = keysym_return;
@@ -557,11 +629,12 @@ Boolean _XtMatchUsingStandardMods (event,eventSeq)
     Boolean resolved = TRUE;
     Display *dpy = eventSeq->xev->xany.display;
     XtPerDisplay pd = _XtGetPerDisplay(dpy);
+    TMContext tm_context = pd->tm_context;
 
-    XtTranslateKeycode( dpy, (KeyCode)eventSeq->event.eventCode,
-		        (Modifiers)(eventSeq->event.modifiers &
-				    ((ShiftMask|LockMask) | pd->mode_switch)),
-		        &modifiers_return, &keysym_return);
+    computed = (Modifiers)(eventSeq->event.modifiers &
+			   ((ShiftMask|LockMask) | pd->mode_switch));
+    TRANSLATE(tm_context, pd, dpy, (KeyCode)eventSeq->event.eventCode,
+	      computed, modifiers_return, keysym_return);
 
     if ((event->eventCode & event->eventCodeMask) ==
              (keysym_return & event->eventCodeMask)) {
@@ -574,8 +647,6 @@ Boolean _XtMatchUsingStandardMods (event,eventSeq)
 
         if ((computed & computedMask) ==
 	    (eventSeq->event.modifiers & ~modifiers_return & computedMask)) {
-	    TMContext tm_context = pd->tm_context;
-	    if (tm_context == NULL) tm_context = AllocTMContext(dpy);
 	    tm_context->event = eventSeq->xev;
 	    tm_context->serial = eventSeq->xev->xany.serial;
 	    tm_context->keysym = keysym_return;
@@ -2783,7 +2854,7 @@ void _XtRegisterGrabs(widget, acceleratorsOnly)
     }
 }
 
-static XtActionsRec Const tmActions[] = {
+static XtActionsRec RConst tmActions[] = {
     {"XtMenuPopup", XtMenuPopupAction},
     {"XtMenuPopdown", _XtMenuPopdownAction},
     {"MenuPopup", XtMenuPopupAction}, /* old & obsolete */
@@ -2819,6 +2890,7 @@ void _XtBuildKeysymTables(dpy,pd)
     KeyCode keycode;
 #define KeysymTableSize 16
 
+    FLUSHKEYCACHE(pd->tm_context);
     if (pd->keysyms)
 	XtFree( (XtPointer)pd->keysyms );
     XDisplayKeycodes(dpy, &pd->min_keycode, &pd->max_keycode);
@@ -2984,7 +3056,10 @@ void XtSetKeyTranslator(dpy, translator)
     XtKeyProc translator;
 
 {
-    _XtGetPerDisplay(dpy)->defaultKeycodeTranslator = translator;
+    XtPerDisplay pd = _XtGetPerDisplay(dpy);
+
+    pd->defaultKeycodeTranslator = translator;
+    FLUSHKEYCACHE(pd->tm_context);
     /* XXX should now redo grabs */
 }
 
@@ -2997,7 +3072,10 @@ void XtRegisterCaseConverter(dpy, proc, start, stop)
     KeySym stop;
 
 {
-    _XtGetPerDisplay(dpy)->defaultCaseConverter = proc;
+    XtPerDisplay pd = _XtGetPerDisplay(dpy);
+
+    pd->defaultCaseConverter = proc;
+    FLUSHKEYCACHE(pd->tm_context);
     /* XXX should now redo grabs */
 }
 
