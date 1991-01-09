f93a0646db1df6fc158a21dd8b258e9e82e575d3
gildea <empty>
1991-01-09 18:30:51 +0000

    cast bitmaps to signed char add copyright notice

--- xc/programs/oclock/oclock.c
+++ xc/programs/oclock/oclock.c
@@ -1,6 +1,25 @@
-#ifndef lint
-static char rcsid[] = "$XConsortium: oclock.c,v 1.10 90/10/25 17:32:24 dave Exp $";
-#endif /* lint */
+/*
+ * $XConsortium$
+ *
+ * Copyright 1989 Massachusetts Institute of Technology
+ *
+ * Permission to use, copy, modify, distribute, and sell this software and its
+ * documentation for any purpose is hereby granted without fee, provided that
+ * the above copyright notice appear in all copies and that both that
+ * copyright notice and this permission notice appear in supporting
+ * documentation, and that the name of M.I.T. not be used in advertising or
+ * publicity pertaining to distribution of the software without specific,
+ * written prior permission.  M.I.T. makes no representations about the
+ * suitability of this software for any purpose.  It is provided "as is"
+ * without express or implied warranty.
+ *
+ * M.I.T. DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING ALL
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO EVENT SHALL M.I.T.
+ * BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
+ * OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN 
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
 
 #include <X11/Intrinsic.h>
 #include <X11/Xatom.h>
@@ -78,12 +97,12 @@ void main(argc, argv)
     XtSetArg (arg[i], XtNiconPixmap, 
 	      XCreateBitmapFromData (XtDisplay(toplevel),
 				     XtScreen(toplevel)->root,
-				     oclock_bits, oclock_width, oclock_height));
+				     (char *)oclock_bits, oclock_width, oclock_height));
     i++;
     XtSetArg (arg[i], XtNiconMask,
 	      XCreateBitmapFromData (XtDisplay(toplevel),
 				     XtScreen(toplevel)->root,
-				     oclmask_bits, oclmask_width, oclmask_height));
+				     (char *)oclmask_bits, oclmask_width, oclmask_height));
     i++;
     XtSetValues (toplevel, arg, i);
 
