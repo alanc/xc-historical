8dcb5533c369466e0b456feabe81f78694e25552
converse <empty>
1991-02-18 16:10:12 +0000

    failure to open display causes core dump #2612 added RCS header line and copyright. set Shell input resource to True

--- xc/unsupported/programs/xgc/main.c
+++ xc/unsupported/programs/xgc/main.c
@@ -1,5 +1,27 @@
-/*
-** xgc
+/* $XConsortium$
+ *
+ * Copyright 1991 Massachusetts Institute of Technology
+ *
+ * Permission to use, copy, modify, distribute, and sell this software and its
+ * documentation for any purpose is hereby granted without fee, provided that
+ * the above copyright notice appear in all copies and that both that
+ * copyright notice and this permission notice appear in supporting
+ * documentation, and that the name of M.I.T. not be used in advertising or
+ * publicity pertaining to distribution of the software without specific,
+ * written prior permission.  M.I.T. makes no representations about the
+ * suitability of this software for any purpose.  It is provided "as is"
+ * without express or implied warranty.
+ *
+ * M.I.T. DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING ALL
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO EVENT SHALL M.I.T.
+ * BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
+ * OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ */
+
+/* xgc
 **
 ** main.c
 **
@@ -92,6 +114,10 @@ main(argc,argv)
      int argc;
      char **argv;
 {
+  static Arg shellargs[] = {
+    {XtNinput, 	      (XtArgVal) True}
+  };
+
   static Arg testformargs[] = {	
     {XtNfromVert,     (XtArgVal) NULL} /* put it under GCform */
   };
@@ -135,12 +161,10 @@ main(argc,argv)
 
   /* Initialize toolkit stuff */
 
-  XtToolkitInitialize();
-  appcontext = XtCreateApplicationContext();
-  X.dpy = XtOpenDisplay(appcontext, NULL, "xgc", "Xgc", NULL, 0,
-			&argc, argv);
-  bigdaddy = XtAppCreateShell(NULL, "Xgc", applicationShellWidgetClass,
-			      X.dpy, NULL, 0);
+  bigdaddy = XtAppInitialize(&appcontext, "Xgc", (XrmOptionDescList) NULL,
+			     (Cardinal) 0, &argc, argv, (String *) NULL,
+			     shellargs, XtNumber(shellargs));
+  X.dpy = XtDisplay(bigdaddy);
 
   /* Initialize GC stuff */
 
