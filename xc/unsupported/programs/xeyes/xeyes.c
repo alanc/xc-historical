106a2efc4a19e6332dc806f61cf9c33409c34049
converse <empty>
1991-02-22 18:02:50 +0000

    make RCS header not a static string add copyright cooperate with WM_DELETE_WINDOW protocol miscellaneous code removed.

--- xc/unsupported/programs/xeyes/xeyes.c
+++ xc/unsupported/programs/xeyes/xeyes.c
@@ -1,9 +1,27 @@
-#ifndef lint
-static char rcsid[] = "$XConsortium: xeyes.c,v 1.11 91/01/09 16:51:44 gildea Exp $";
-#endif /* lint */
+/* $XConsortium: xeyes.c,v 1.12 91/01/26 14:37:22 rws Exp $ */
+/*
+ * Copyright 1991 Massachusetts Institute of Technology
+ *
+ * Permission to use, copy, modify, distribute, and sell this software and its
+ * documentation for any purpose is hereby granted without fee, provided that
+ * the above copyright notice appear in all copies and that both that
+ * copyright notice and this permission notice appear in supporting
+ * documentation, and that the name of M.I.T. not be used in advertising or
+ * publicity pertaining to distribution of the software without specific,
+ * written prior permission.  M.I.T. makes no representations about the
+ * suitability of this software for any purpose.  It is provided "as is"
+ * without express or implied warranty.
+ *
+ * M.I.T. DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING ALL
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO EVENT SHALL M.I.T.
+ * BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
+ * OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ */
 
 #include <X11/Intrinsic.h>
-#include <X11/Xatom.h>
 #include <X11/StringDefs.h>
 #include <X11/Shell.h>
 #include "Eyes.h"
@@ -12,9 +30,7 @@ static char rcsid[] = "$XConsortium: xeyes.c,v 1.11 91/01/09 16:51:44 gildea Exp
 #include "eyesmask.bit"
 
 extern void exit();
-
-/* Command line options table.  Only resources are entered here...there is a
-   pass over the remaining options after XtParseCommand is let loose. */
+static void quit();
 
 /* Exit with message describing command line format */
 
@@ -33,6 +49,9 @@ void usage()
     exit(1);
 }
 
+/* Command line options table.  Only resources are entered here...there is a
+   pass over the remaining options after XtParseCommand is let loose. */
+
 static XrmOptionDescRec options[] = {
 {"-outline",	"*eyes.outline",	XrmoptionSepArg,	NULL},
 {"-center",	"*eyes.center",		XrmoptionSepArg,	NULL},
@@ -40,20 +59,29 @@ static XrmOptionDescRec options[] = {
 {"-shape",	"*eyes.shapeWindow",	XrmoptionNoArg,		"TRUE"},
 };
 
+static XtActionsRec actions[] = {
+    {"quit",	quit}
+};
+
+static Atom wm_delete_window;
+
 void main(argc, argv)
     int argc;
     char **argv;
 {
+    XtAppContext app_context;
     Widget toplevel;
     Widget eyes;
     Arg arg[2];
-    char *labelname = NULL;
-    int	i;
+    Cardinal i;
     
-    toplevel = XtInitialize("main", "XEyes", options, XtNumber (options),
+    toplevel = XtInitialize(NULL, "XEyes", options, XtNumber(options), 
 			    &argc, argv);
-      
     if (argc != 1) usage();
+    app_context = XtWidgetToApplicationContext(toplevel);
+    XtAppAddActions(app_context, actions, XtNumber(actions));
+    XtOverrideTranslations
+	(toplevel, XtParseTranslationTable ("<Message>WM_PROTOCOLS: quit()"));
     
     i = 0;
     XtSetArg (arg[i], XtNiconPixmap, 
@@ -69,9 +97,26 @@ void main(argc, argv)
     i++;
     XtSetValues (toplevel, arg, i);
 
-    XtSetArg (arg[0], XtNlabel, &labelname);
     eyes = XtCreateManagedWidget ("eyes", eyesWidgetClass, toplevel, NULL, 0);
-    XtGetValues(eyes, arg, 1);
     XtRealizeWidget (toplevel);
-    XtMainLoop();
+    wm_delete_window = XInternAtom(XtDisplay(toplevel), "WM_DELETE_WINDOW",
+				   False);
+    (void) XSetWMProtocols (XtDisplay(toplevel), XtWindow(toplevel),
+                            &wm_delete_window, 1);
+    XtAppMainLoop(app_context);
+}
+
+static void quit(w, event, params, num_params)
+    Widget w;
+    XEvent *event;
+    String *params;
+    Cardinal *num_params;
+{
+    if (event->type == ClientMessage && 
+	event->xclient.data.l[0] != wm_delete_window) {
+	XBell(XtDisplay(w), 0);
+    } else {
+	XCloseDisplay(XtDisplay(w));
+	exit(0);
+    }
 }
