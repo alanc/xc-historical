6843d853e795b3f5f8147a9b6bd13cdb4db857cb
rws <empty>
1991-03-23 21:56:51 +0000

    remove needless defined(FUNCPROTO)

--- xc/include/Xfuncproto.h
+++ xc/include/Xfuncproto.h
@@ -1,4 +1,4 @@
-/* $XConsortium: Xfuncproto.h,v 1.2 91/02/17 11:14:35 rws Exp $ */
+/* $XConsortium: Xfuncproto.h,v 1.3 91/03/23 14:43:26 rws Exp $ */
 /* 
  * Copyright 1989, 1991 by the Massachusetts Institute of Technology
  *
@@ -28,7 +28,7 @@
 #endif /* NeedFunctionPrototypes */
 
 #ifndef NeedVarargsPrototypes
-#if __STDC__ || defined(__cplusplus) || defined(c_plusplus) || (defined(FUNCPROTO) && FUNCPROTO>1)
+#if __STDC__ || defined(__cplusplus) || defined(c_plusplus) || FUNCPROTO>1
 #define NeedVarargsPrototypes 1
 #else
 #define NeedVarargsPrototypes 0
