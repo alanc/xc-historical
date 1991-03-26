cbb9465f04834ef2c8f03679fec8ba7808259156
rws <empty>
1991-03-26 08:34:47 +0000

    simplify put in stupid High C stuff

--- xc/lib/X11/Xvarargs.h
+++ xc/lib/X11/Xvarargs.h
@@ -1,30 +1,61 @@
+/* $XConsortium: Xvarargs.h,v 1.12 91/03/23 11:25:21 rws Exp $ */
+/*
+
+Copyright 1985, 1986, 1987, 1988, 1989 by the
+Massachusetts Institute of Technology
+
+Permission to use, copy, modify, and distribute this
+software and its documentation for any purpose and without
+fee is hereby granted, provided that the above copyright
+notice appear in all copies and that both that copyright
+notice and this permission notice appear in supporting
+documentation, and that the name of M.I.T. not be used in
+advertising or publicity pertaining to distribution of the
+software without specific, written prior permission.
+M.I.T. makes no representations about the suitability of
+this software for any purpose.  It is provided "as is"
+without express or implied warranty.
+
+*/
+
 #ifndef _XVARARGS_H_
 #define _XVARARGS_H_
 
-#if __STDC__
-# ifndef NOSTDHDRS
-#  define _XSTDVAR
-# else
-# ifdef __GNUC__
-#  define _XSTDVAR
-# endif
-# endif
-#else
-#ifdef sgi
-#define _XSTDVAR
-#endif
-#endif
+#ifdef __HIGHC__
+#ifndef _STDARG_H
+#define _STDARG_H
 
-#ifdef _XSTDVAR
+typedef char *va_list;
+
+/* Amount of space required in an argument list for an arg of type TYPE.
+   TYPE may alternatively be an expression whose type is used.  */
+
+#define __va_rounded_size(TYPE)  \
+  (((sizeof (TYPE) + sizeof (int) - 1) / sizeof (int)) * sizeof (int))
+
+#define va_start(AP, LASTARG)                                           \
+ (AP = ((char *) &(LASTARG) + __va_rounded_size (LASTARG)))
+
+#define va_end(AP)
+
+#define va_arg(AP, TYPE)                                                \
+ (AP += __va_rounded_size (TYPE),                                       \
+  *((TYPE *) (AP - __va_rounded_size (TYPE))))
+
+#endif /* _STDARG_H */
+
+#define Va_start(a,b) va_start(a,b)
+
+#else /* !__HIGHC__ */
+
+#if NeedVarargsPrototypes
 # include <stdarg.h>
 # define Va_start(a,b) va_start(a,b)
 #else
-# if NeedVarargsPrototypes
-#  undef NeedVarargsPrototypes
-#  define NeedVarargsPrototypes 0
-# endif
 # include <varargs.h>
 # define Va_start(a,b) va_start(a)
 #endif
+
+#endif /* __HIGHC__ */
 
 #endif /* _XVARARGS_H_ */
