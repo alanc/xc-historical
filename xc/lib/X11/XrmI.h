7d4bde1c776ca27f93e01df64c13152b994b32ce
rws <empty>
1991-04-23 18:25:52 +0000

    put only OS stuff here, move the rest to Xrm.c

--- xc/lib/X11/XrmI.h
+++ xc/lib/X11/XrmI.h
@@ -1,4 +1,4 @@
-/* $XConsortium: XrmI.h,v 1.5 90/08/23 09:48:35 rws Exp $ */
+/* $XConsortium: XrmI.h,v 1.6 90/12/01 16:04:54 rws Exp $ */
 /*
 
 Copyright 1990 by the Massachusetts Institute of Technology
@@ -15,40 +15,6 @@ without express or implied warranty.
 
 */
 
-#define NUM_CHARS 256
-
-#define XrmBits unsigned char
-
-#define _EOF   	((XrmBits) (1 << 0))
-#define SPACE 	((XrmBits) (1 << 1))
-#define SEP 	((XrmBits) (1 << 2))
-#define TIGHT 	((XrmBits) (1 << 3))
-#define LOOSE 	((XrmBits) (1 << 4))
-#define EOL	((XrmBits) (1 << 5))
-#define ODIGIT	((XrmBits) (1 << 6))
-#define BSLASH  ((XrmBits) (1 << 7))
-
-#define A_NEW_LINE 	('\n')
-#define BACKSLASH 	('\\')
-#define STRING_TERMINATOR ('\0')
-#define ZERO		('0')
-
-#define get_next_char(ch, str) (_xrmtypes[(unsigned char)((ch) = *((str)++))])
-
-#define xrm_is_space(bits) 		((bits) & SPACE)
-#define xrm_is_end_of_quark(bits)     ((bits)&(LOOSE|TIGHT|SEP|SPACE|_EOF|EOL))
-#define xrm_is_EOL(bits) 		((bits) & (EOL|_EOF))
-#define xrm_is_EOF(bits) 		((bits) & _EOF)
-#define xrm_is_tight(bits)		((bits) & TIGHT)
-#define xrm_is_loose(bits)		((bits) & LOOSE)
-#define xrm_is_tight_or_loose(bits)     ((bits) & (TIGHT|LOOSE))
-#define xrm_is_odigit(bits)		((bits) & ODIGIT)
-#define xrm_is_separator(bits) 		((bits) & (SEP|SPACE|_EOF|EOL))
-#define xrm_is_real_separator(bits) 	((bits) & SEP)
-#define xrm_is_end_of_value(bits) 	((bits) & (EOL|_EOF))
-#define xrm_is_backslash(bits)		((bits) & BSLASH)
-#define xrm_is_backslash_or_EOV(bits)   ((bits) & (EOL|_EOF|BSLASH))
-
 /*
  * Macros to abstract out reading the file, and getting its size.
  *
@@ -60,8 +26,8 @@ without express or implied warranty.
 
 #define OpenFile(name) 		open((name), O_RDONLY)
 #define CloseFile(fd)           close((fd))
-#define ReadFile(fd, buf, size) read((fd), (buf), (size))
-#define GetSizeOfFile(name, size)                   \
+#define ReadFile(fd,buf,size)	read((fd), (buf), (size))
+#define GetSizeOfFile(name,size)                    \
 {                                                   \
     struct stat status_buffer;                      \
     if ( (stat((name), &status_buffer)) == -1 )     \
@@ -69,4 +35,3 @@ without express or implied warranty.
     else                                            \
 	size = status_buffer.st_size;               \
 }
-
