6ae8d41ce0793fdce5f72914f6f7fba416ad29c3
keith <empty>
1991-05-04 11:52:53 +0000

    fix pointer usage

--- xc/programs/Xserver/cfb/cfbtegblt.c
+++ xc/programs/Xserver/cfb/cfbtegblt.c
@@ -1,4 +1,4 @@
-/* $XConsortium: cfbtegblt.c,v 5.2 91/01/27 13:02:51 keith Exp $ */
+/* $XConsortium: cfbtegblt.c,v 5.3 91/04/10 11:41:31 keith Exp $ */
 /***********************************************************
 Copyright 1987 by Digital Equipment Corporation, Maynard, Massachusetts,
 and the Massachusetts Institute of Technology, Cambridge, Massachusetts.
@@ -77,12 +77,13 @@ cfbTEGlyphBlt(pDrawable, pGC, x, y, nglyph, ppci, pglyphBase)
     register unsigned char *pglyph;
     int widthGlyph;
 
-    register unsigned int *pdst;/* pointer to current longword in dst */
+    register unsigned long *pdst;/* pointer to current longword in dst */
     int hTmp;			/* counter for height */
     BoxRec bbox;		/* for clipping */
 
     register int wtmp,xtemp,width;
-    unsigned int bgfill,fgfill,*ptemp,tmpDst1,tmpDst2,tmpx,*pdtmp;
+    unsigned long bgfill,fgfill,*ptemp,tmpDst1,tmpDst2,*pdtmp;
+    int tmpx;
 
     xpos += pDrawable->x;
     ypos += pDrawable->y;
@@ -154,13 +155,13 @@ cfbTEGlyphBlt(pDrawable, pGC, x, y, nglyph, ppci, pglyphBase)
 		    w = min(width, PPW - tmpx);
 		    w = min(w, (32 - xtemp));
 
-		    ptemp = (unsigned int *)(pglyph + (xtemp >> 5));
+		    ptemp = (unsigned long *)(pglyph + (xtemp >> 5));
 		    getstipplepixels(ptemp,xtemp,w,0,&bgfill,&tmpDst1);
 		    getstipplepixels(ptemp,xtemp,w,1,&fgfill,&tmpDst2);
 
 		    {
-			int tmpDst = tmpDst1 | tmpDst2;
-			unsigned int *pdsttmp = pdst + (x >> PWSH);
+			unsigned long tmpDst = tmpDst1 | tmpDst2;
+			unsigned long *pdsttmp = pdst + (x >> PWSH);
 			putbits(tmpDst,tmpx,w,pdsttmp,pGC->planemask);
 		    }
 		    x += w;
