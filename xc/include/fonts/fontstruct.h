5f515b9c51a9ba3359220ef7c3a37b212ed74d24
keith <empty>
1991-05-10 11:39:44 +0000

    add reset_fpe function

--- xc/include/fonts/fontstruct.h
+++ xc/include/fonts/fontstruct.h
@@ -1,17 +1,17 @@
-/* $Header: fontstruct.h,v 1.1 91/02/20 19:42:28 keith Exp $ */
+/* $Header: fontstruct.h,v 1.2 91/02/22 21:51:53 keith Exp $ */
 /***********************************************************
 Copyright 1987 by Digital Equipment Corporation, Maynard, Massachusetts,
 and the Massachusetts Institute of Technology, Cambridge, Massachusetts.
 
                         All Rights Reserved
 
-Permission to use, copy, modify, and distribute this software and its 
-documentation for any purpose and without fee is hereby granted, 
+Permission to use, copy, modify, and distribute this software and its
+documentation for any purpose and without fee is hereby granted,
 provided that the above copyright notice appear in all copies and that
-both that copyright notice and this permission notice appear in 
+both that copyright notice and this permission notice appear in
 supporting documentation, and that the names of Digital or MIT not be
 used in advertising or publicity pertaining to distribution of the
-software without specific, written prior permission.  
+software without specific, written prior permission.
 
 DIGITAL DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING
 ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO EVENT SHALL
@@ -20,6 +20,8 @@ ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
 WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION,
 ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
 SOFTWARE.
+
+	@(#)fontstruct.h	3.2	91/04/15
 
 ******************************************************************/
 
@@ -38,28 +40,30 @@ SOFTWARE.
 
 #define GLYPHPADOPTIONS 4	/* 1, 2, 4, or 8 */
 
-typedef enum {Linear8Bit, TwoD8Bit, Linear16Bit, TwoD16Bit} FontEncoding;
+typedef enum {
+    Linear8Bit, TwoD8Bit, Linear16Bit, TwoD16Bit
+}           FontEncoding;
 
 typedef struct _FontProp {
-    long name;
-    long value;	/* assumes ATOM is not larger than INT32 */
-} FontPropRec;
+    long        name;
+    long        value;		/* assumes ATOM is not larger than INT32 */
+}           FontPropRec;
 
 typedef struct _ExtentInfo {
-    DrawDirection	drawDirection;
-    int			fontAscent;
-    int			fontDescent;
-    int			overallAscent;
-    int			overallDescent;
-    int			overallWidth;
-    int			overallLeft;
-    int			overallRight;
-} ExtentInfoRec;
+    DrawDirection drawDirection;
+    int         fontAscent;
+    int         fontDescent;
+    int         overallAscent;
+    int         overallDescent;
+    int         overallWidth;
+    int         overallLeft;
+    int         overallRight;
+}           ExtentInfoRec;
 
 typedef struct _CharInfo {
-    xCharInfo	metrics;	/* info preformatted for Queries */
-    char	*bits;		/* pointer to glyph image */
-} CharInfoRec;
+    xCharInfo   metrics;	/* info preformatted for Queries */
+    char       *bits;		/* pointer to glyph image */
+}           CharInfoRec;
 
 /*
  * Font is created at font load time. It is specific to a single encoding.
@@ -67,76 +71,88 @@ typedef struct _CharInfo {
  */
 
 typedef struct _FontInfo {
-    unsigned short	firstCol;
-    unsigned short	lastCol;
-    unsigned short	firstRow;
-    unsigned short	lastRow;
-    unsigned short	defaultCh;
-    unsigned int	noOverlap : 1;
-    unsigned int	terminalFont : 1;
-    unsigned int	constantMetrics : 1;
-    unsigned int	constantWidth : 1;
-    unsigned int	inkInside : 1;
-    unsigned int	inkMetrics : 1;
-    unsigned int	allExist : 1;
-    unsigned int	drawDirection : 2;
-    short		maxOverlap;
-    xCharInfo		maxbounds;
-    xCharInfo		minbounds;
-    xCharInfo		ink_maxbounds;
-    xCharInfo		ink_minbounds;
-    short		fontAscent;
-    short		fontDescent;
-    int			nprops;
-    FontPropPtr		props;
-    char		*isStringProp;
-} FontInfoRec;
+    unsigned short firstCol;
+    unsigned short lastCol;
+    unsigned short firstRow;
+    unsigned short lastRow;
+    unsigned short defaultCh;
+    unsigned int noOverlap:1;
+    unsigned int terminalFont:1;
+    unsigned int constantMetrics:1;
+    unsigned int constantWidth:1;
+    unsigned int inkInside:1;
+    unsigned int inkMetrics:1;
+    unsigned int allExist:1;
+    unsigned int drawDirection:2;
+    short       maxOverlap;
+    short       pad;
+    xCharInfo   maxbounds;
+    xCharInfo   minbounds;
+    xCharInfo   ink_maxbounds;
+    xCharInfo   ink_minbounds;
+    short       fontAscent;
+    short       fontDescent;
+    int         nprops;
+    FontPropPtr props;
+    char       *isStringProp;
+}           FontInfoRec;
 
 typedef struct _Font {
-    int			refcnt;
-    FontInfoRec		info;
-    char		bit;
-    char		byte;
-    char		glyph;
-    char		scan;
-    int			(*GetGlyphs)(/* font, count, chars, fontEncoding, glyphcount, glyphs */);
-    int			(*GetMetrics)(/* font, count, chars, fontEncoding, glyphcount, glyphs */);
-    void		(*UnloadFont) (/* font */);
-    FontPathElementPtr  fpe;
-    pointer		fontPrivate;
-    pointer		fpePrivate;
-} FontRec;
+    int         refcnt;
+    FontInfoRec info;
+    char        bit;
+    char        byte;
+    char        glyph;
+    char        scan;
+    fsBitmapFormat format;
+    int         (*get_glyphs) ( /* font, count, chars, encoding, count, glyphs */ );
+    int         (*get_metrics) ( /* font, count, chars, encoding, count, glyphs */ );
+    int         (*get_bitmaps) (/* client, font, flags, ranges, nranges,
+				    nextents, extents */ );
+    int         (*get_extents) (/* client, font, format, flags, ranges,
+				    nranges, nglyphs, offsets, glyphs */ );
+    void        (*unload_font) ( /* font */ );
+    FontPathElementPtr fpe;
+    pointer     svrPrivate;
+    pointer     fontPrivate;
+    pointer     fpePrivate;
+}           FontRec;
 
 typedef struct _FontNames {
-    int			nnames;
-    int			size;
-    int			*length;
-    char		**names;
-} FontNamesRec;
+    int         nnames;
+    int         size;
+    int        *length;
+    char      **names;
+}           FontNamesRec;
 
 /* External view of font paths */
 typedef struct _FontPathElement {
     int         name_length;
-    char	*name;
+    char       *name;
     int         type;
-    int		refcount;
-    char	*private;
-} FontPathElementRec;
+    int         refcount;
+    pointer     private;
+}           FontPathElementRec;
 
 typedef struct _FPEFunctions {
-    int		(*name_check) (/* name */ );
-    int         (*open_font) (/* client, fpe, name, namelen, flags, ppfont */);
-    int		(*close_font) (/* client, fpe, pfont */);
-    int         (*wakeup_fpe) ( /* fpe */ );
-    int         (*list_fonts) (	/* closure, fpe, pattern, patlen, maxnames,
-			           list_info */ );
-    int         (*start_list_fonts_with_info) ( /* closure, fpe */ );
-    int         (*list_next_font_with_info) ( /* closure, fpe */ );
+    int         (*name_check) ( /* name */ );
     int         (*init_fpe) ( /* fpe */ );
+    int         (*reset_fpe) ( /* fpe */ );
     int         (*free_fpe) ( /* fpe */ );
-} FPEFunctionsRec, FPEFunctions;
+    int         (*open_font) (	/* client, fpe, flags, name, namelen, format,
+			          fid,  ppfont, alias */ );
+    int         (*close_font) ( /* pfont */ );
+    int         (*list_fonts) (	/* client, fpe, pattern, patlen, maxnames,
+			           paths */ );
+    int         (*start_list_fonts_with_info) (	/* client, fpe, name, namelen,
+					           maxnames, data */ );
+    int         (*list_next_font_with_info) (	/* client, fpe, name, namelen,
+					         info, num, data */ );
+    int         (*wakeup_fpe) ( /* fpe, mask */ );
+    FontNamesPtr renderer_names;
+}           FPEFunctionsRec, FPEFunctions;
 
-extern int	FontRegisterFPEFunctions();
+extern int  InitFPETypes();
 
 /*
  * Various macros for computing values based on contents of
@@ -162,5 +178,4 @@ extern int	FontRegisterFPEFunctions();
 
 #define BYTES_FOR_GLYPH(ci,pad)	(GLYPHHEIGHTPIXELS(ci) * \
 				 BYTES_PER_ROW(GLYPHWIDTHPIXELS(ci),pad))
-
-#endif /* FONTSTR_H */
+#endif				/* FONTSTR_H */
