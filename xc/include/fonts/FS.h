0498c57569ff7b6866789f2dbf221bedeee4899f
keith <empty>
1991-05-11 09:25:30 +0000

    use fsmasks.h

--- xc/include/fonts/FS.h
+++ xc/include/fonts/FS.h
@@ -26,6 +26,8 @@
 #ifndef _FS_H_
 #define	_FS_H_
 
+#include <fsmasks.h>
+
 #define	FS_PROTOCOL		1
 #define	FS_PROTOCOL_MINOR	0
 
@@ -45,42 +47,12 @@ typedef FSID	AccContext;
 typedef unsigned int    FSDrawDirection;
 #endif
 
+#ifndef None
 #define	None		0L
+#endif
 
 #define	LeftToRightDrawDirection	0
 #define	RightToLeftDrawDirection	1
-
-/* font format macros */
-#define	BitmapFormatByteOrderMask	(1L << 0)
-#define	BitmapFormatBitOrderMask	(1L << 1)
-#define	BitmapFormatImageRectMask	(3L << 2)
-#define	BitmapFormatScanlinePadMask	(3L << 8)
-#define	BitmapFormatScanlineUnitMask	(3L << 12)
-
-#define	BitmapFormatByteOrderLSB	(0)
-#define	BitmapFormatByteOrderMSB	(1L << 0)
-#define	BitmapFormatBitOrderLSB		(0)
-#define	BitmapFormatBitOrderMSB		(1L << 1)
-
-#define	BitmapFormatImageRectMin	(0L << 2)
-#define	BitmapFormatImageRectMaxWidth   (1L << 2)
-#define	BitmapFormatImageRectMax        (2L << 2)
-
-#define	BitmapFormatScanlinePad8	(0L << 8)
-#define	BitmapFormatScanlinePad16	(1L << 8)
-#define	BitmapFormatScanlinePad32	(2L << 8)
-#define	BitmapFormatScanlinePad64	(3L << 8)
-
-#define	BitmapFormatScanlineUnit8	(0L << 12)
-#define	BitmapFormatScanlineUnit16	(1L << 12)
-#define	BitmapFormatScanlineUnit32	(2L << 12)
-#define	BitmapFormatScanlineUnit64	(3L << 12)
-
-#define	BitmapFormatMaskByte		(1L << 0)
-#define	BitmapFormatMaskBit		(1L << 1)
-#define	BitmapFormatMaskImageRectangle	(1L << 2)
-#define	BitmapFormatMaskScanLinePad	(1L << 3)
-#define	BitmapFormatMaskScanLineUnit	(1L << 4)
 
 /* font info flags */
 #define	FontInfoAllCharsExist		(1L << 0)
@@ -98,9 +70,11 @@ typedef unsigned int    FSDrawDirection;
 #define	PropTypeUnsigned	1
 #define	PropTypeSigned		2
 
+#ifndef LSBFirst
 /* byte order */
 #define LSBFirst                0
 #define MSBFirst                1
+#endif
 
 /* event masks */
 #define	CatalogueChangeNotifyMask	(1L << 0)
