4ea42f6b78c9f5f00230df78b9d260ff79be8ee1
converse <empty>
1991-05-30 19:06:17 +0000

    Move Athena headers into toolkitaw.c, they are needed no where else. Clean up this mess, deleting blank lines and stupid decorative comments.

--- xc/unsupported/test/InsPEX/tools/inspector/toolkitaw.h
+++ xc/unsupported/test/InsPEX/tools/inspector/toolkitaw.h
@@ -1,5 +1,4 @@
-
-/* $XConsortium$ */
+/* $XConsortium: toolkitaw.h,v 5.1 91/02/16 09:59:45 rws Exp $ */
 
 /*****************************************************************
 Copyright (c) 1989,1990, 1991 by Sun Microsystems, Inc. and the X Consortium.
@@ -25,79 +24,37 @@ SOFTWARE.
 
 ******************************************************************/
 
-
 /*
  * toolkitaw.h  -- defines the toolkit-independent interface used
- * by the comparison tool.  This implementation uses Athena Widgets
- *
- * See top block comment of toolkit.c for comments on design of this
- * interface.  See top comment of toolkitaw.c for comments on
- * the athena widgets implementation.
+ * by the comparison tool.  This implementation uses Athena Widgets.
  */
-
 #include <X11/Intrinsic.h>
-#include <X11/StringDefs.h>
-#include <X11/Xaw/Command.h>
-#include <X11/Xaw/Box.h>
-#include <X11/Xaw/Form.h>
-#include <X11/Xaw/List.h>
-#include <X11/Xaw/Viewport.h>
 
 /* note that these button status values are not arbitrary - come from XView */
 #define TK_BUTTON_ACTIVE    0
 #define TK_BUTTON_INACTIVE  1
 
-/*
-**
-**  Defining some toolkit-independent data types
-**
-*/
-
-/*
- * TK_Main_Window - an application's main window.
- */
-typedef	Widget TK_Main_Window;	/* Box widget */
-
-/*
- * TK_Control_Window - A subwindow in which buttons, text input lines,
- * and other control devices may be placed.
- */
-typedef Widget TK_Control_Window;  /* Form widget */
-
-/*
- * Kinds of interface items
- */
+typedef	Widget TK_Main_Window;
+typedef Widget TK_Control_Window;
 typedef Widget TK_Button;
 typedef Widget TK_List;
-
-/*
- * TK_Message_Item - identifies output message item so string can
- * be changed
- */
 typedef Widget TK_Message_Item;
 
+/*  Toolkit-independent functions --
+ *  See toolkit.c for descriptions & parameter types
+ */
 
-/*
-**
-**  Toolkit-independent functions --
-**  See toolkit.c for descriptions & parameter types
-**
-*/
-
-void		    tk_init();
-
+void 	    	    tk_init();
 TK_Main_Window	    tk_create_main_window();
 TK_Control_Window   tk_create_control_window();
 Drawable	    tk_create_X_drawable_window();
 XID		    tk_get_xid();
-
 TK_Button	    tk_create_button();
 void		    tk_activate_button();
 void		    tk_deactivate_button();
 void		    tk_set_button_label();
 TK_Message_Item	    tk_create_message_item();
 void		    tk_set_message();
-
 TK_List		    tk_create_list();
 char *		    tk_get_selected_list_item();
 char *		    tk_get_list_item();
@@ -105,7 +62,6 @@ int		    tk_get_list_length();
 void		    tk_insert_list_item();
 void		    tk_set_list_item();
 void		    tk_delete_list_item();
-
 void		    tk_main_loop();
 
 
