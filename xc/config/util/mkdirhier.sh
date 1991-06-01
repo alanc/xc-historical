00cd1d2e3c3b55a7fd28b66a220e70814d917b8e
rws <empty>
1991-06-01 10:15:42 +0000

    new version

--- xc/config/util/mkdirhier.sh
+++ xc/config/util/mkdirhier.sh
@@ -1,24 +1,58 @@
 #!/bin/sh
+# Courtesy of Paul Eggert
 
-#
-# create a heirarchy of directories
-#
+newline='
+'
+IFS=$newline
 
-for f in $*; do
-    parts=`echo $f | sed 's,\(.\)/\(.\),\1 \2,g' | sed 's,/$,,'`;
-    path="";
-    for p in $parts; do
-	if [ x"$path" = x ]; then
-	    dir=$p;
-	else
-	    dir=$path/$p;
-	fi;
-	if [ ! -d $dir ]; then
-	    echo mkdir $dir; 
-	    mkdir $dir;
-	    chmod a+rx $dir; 
-	fi;
-	path=$dir;
-    done;
-done
+case ${1--} in
+-*) echo >&2 "mkdirhier: usage: mkdirhier directory ..."; exit 1
+esac
 
+status=
+
+for directory
+do
+	case $directory in
+	'')
+		echo >&2 "mkdirhier: empty directory name"
+		status=1
+		continue;;
+	*"$newline"*)
+		echo >&2 "mkdirhier: directory name contains a newline: \`\`$directory''"
+		status=1
+		continue;;
+	///*) prefix=/;; # See Posix 2.3 "path".
+	//*) prefix=//;;
+	/*) prefix=/;;
+	-*) prefix=./;;
+	*) prefix=
+	esac
+
+	IFS=/
+	set x $directory
+	IFS=$newline
+	shift
+
+	for filename
+	do
+		path=$prefix$filename
+		prefix=$path/
+		shift
+
+		test -d "$path" || {
+			paths=$path
+			for filename
+			do
+				path=$path/$filename
+				paths=$paths$newline$path
+			done
+
+			mkdir $paths || status=$?
+
+			break
+		}
+	done
+  done
+
+exit $status
