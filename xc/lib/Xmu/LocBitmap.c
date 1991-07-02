396944314653aebd08acf01190f78be6fe68073d
rws <empty>
1991-07-02 09:09:59 +0000

    fix nit in PATH_MAX stuff

--- xc/lib/Xmu/LocBitmap.c
+++ xc/lib/Xmu/LocBitmap.c
@@ -1,5 +1,5 @@
 /*
- * $XConsortium: LocBitmap.c,v 1.14 91/05/21 11:17:01 converse Exp $
+ * $XConsortium: LocBitmap.c,v 1.15 91/05/28 16:36:33 converse Exp $
  *
  * Copyright 1989 Massachusetts Institute of Technology
  *
@@ -37,16 +37,15 @@
 #include <limits.h>
 #undef _POSIX_SOURCE
 #endif
-#endif
+#endif /* X_NOT_POSIX */
 #ifndef PATH_MAX
 #include <sys/param.h>
 #ifdef MAXPATHLEN
 #define PATH_MAX MAXPATHLEN
-#endif
-#endif
-#ifndef PATH_MAX
+#else
 #define PATH_MAX 1024
 #endif
+#endif /* PATH_MAX */
 
 static char **split_path_string();
 
