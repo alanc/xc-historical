dd880ed1c931d001c27ce4bce022764d7ae54788
rws <empty>
1991-07-13 20:54:21 +0000

    use XrmEnumerateDatabase permit class/instance prefix

--- xc/programs/appres/appres.c
+++ xc/programs/appres/appres.c
@@ -1,5 +1,5 @@
 /*
- * $XConsortium: appres.c,v 1.7 91/01/10 13:56:54 gildea Exp $
+ * $XConsortium: appres.c,v 1.8 91/05/05 12:37:05 gildea Exp $
  *
  * Copyright 1989 Massachusetts Institute of Technology
  *
@@ -26,14 +26,90 @@
 #include <X11/Intrinsic.h>
 #include <stdio.h>
 
-#define NOCLASS "-NoSuchClass-"
+#define NONAME "-AppResTest-"
 
 char *ProgramName;
 
+XrmQuark XrmQString;
+
 static void usage ()
 {
-    fprintf (stderr, "usage:  %s  [class [instance]]\n", ProgramName);
+    fprintf (stderr,
+	     "usage:  %s  [classprefix [instanceprefix]] [-1] [toolkitoptions]\n",
+	     ProgramName);
+    fprintf (stderr,
+	     "-1      list resources only at the specified level\n");
+    fprintf (stderr,
+	     "The number of class and instance elements must be equal.\n");
     exit (1);
+}
+
+/* stolen from Xlib Xrm.c */
+static void PrintBindingQuarkList(bindings, quarks, stream)
+    XrmBindingList      bindings;
+    XrmQuarkList	quarks;
+    FILE		*stream;
+{
+    Bool	firstNameSeen;
+
+    for (firstNameSeen = False; *quarks; bindings++, quarks++) {
+	if (*bindings == XrmBindLoosely) {
+	    (void) fprintf(stream, "*");
+	} else if (firstNameSeen) {
+	    (void) fprintf(stream, ".");
+	}
+	firstNameSeen = True;
+	(void) fputs(XrmQuarkToString(*quarks), stream);
+    }
+}
+
+/* stolen from Xlib Xrm.c */
+/* output out the entry in correct file syntax */
+/*ARGSUSED*/
+static Bool DumpEntry(db, bindings, quarks, type, value, data)
+    XrmDatabase		*db;
+    XrmBindingList      bindings;
+    XrmQuarkList	quarks;
+    XrmRepresentation   *type;
+    XrmValuePtr		value;
+    XPointer		data;
+{
+    FILE			*stream = (FILE *)data;
+    register unsigned int	i;
+    register char		*s;
+    register char		c;
+
+    if (*type != XrmQString)
+	(void) putc('!', stream);
+    PrintBindingQuarkList(bindings, quarks, stream);
+    s = value->addr;
+    i = value->size;
+    if (*type == XrmQString) {
+	(void) fputs(":\t", stream);
+	if (i)
+	    i--;
+    }
+    else
+	fprintf(stream, "=%s:\t", XrmRepresentationToString(*type));
+    if (i && (*s == ' ' || *s == '\t'))
+	(void) putc('\\', stream); /* preserve leading whitespace */
+    while (i--) {
+	c = *s++;
+	if (c == '\n') {
+	    if (i)
+		(void) fputs("\\n\\\n", stream);
+	    else
+		(void) fputs("\\n", stream);
+	} else if (c == '\\')
+	    (void) fputs("\\\\", stream);
+	else if ((c < ' ' && c != '\t') ||
+		 ((unsigned char)c >= 0x7f && (unsigned char)c < 0xa0))
+	    (void) fprintf(stream, "\\%03o", (unsigned char)c);
+	else
+	    (void) putc(c, stream);
+    }
+    (void) putc('\n', stream);
+    return False;
 }
 
 main (argc, argv)
@@ -41,93 +117,50 @@ main (argc, argv)
     char **argv;
 {
     Widget toplevel;
-    char *iname = NULL, *cname = NULL;
-    char tmpbuf[1025];
-    char buf[BUFSIZ];
-    FILE *fp;
-    Bool incont, printit;
-    int clen, ilen;
+    char *iname = NONAME, *cname = NONAME;
     XtAppContext xtcontext;
+    XrmName names[101];
+    XrmClass classes[101];
+    int i;
+    int mode = XrmEnumAllLevels;
 
     ProgramName = argv[0];
-    if (argc > 1) {
-	int newargc = 1;
-	char **newargv = (char **) XtCalloc (argc + 1, sizeof(char *));
-
-	if (newargv) {
-	    int i;
-	    newargv[0] = argv[0];
-	    for (i = 1; i < argc; i++) {
-		if (argv[i][0] != '-') {
-		    if (!cname) {
-			cname = argv[i];
-			continue;
-		    } else if (!iname) {
-			iname = argv[i];
-			continue;
-		    } 
-		}
-		newargv[newargc++] = argv[i];
-	    }
-	    if (iname) newargv[0] = iname;
-	    argc = newargc;
-	    argv = newargv;
-	}
-    }
-    toplevel = XtAppInitialize(&xtcontext, cname ? cname : NOCLASS, NULL, 0,
-			       &argc, argv, NULL, NULL, 0);
-    if (argc != 1) usage ();
-
-    strcpy (tmpbuf, "/tmp/appres.XXXXXX");
-    mktemp (tmpbuf);
-
-    unlink (tmpbuf);
-    XrmPutFileDatabase (XtDatabase(XtDisplay(toplevel)), tmpbuf);
-    
-    fp = fopen (tmpbuf, "r");
-    if(fp == NULL) {
-	fprintf(stderr, "%s: cannot open temp file\n", ProgramName);
-	perror(tmpbuf);
-	exit(1);
-    }
-    unlink (tmpbuf);
-    printit = False;
-    incont = False;
-    if (!cname) cname = "";
-    if (!iname) iname = "";
-    clen = strlen (cname);
-    ilen = strlen (iname);
-    while (fgets (buf, sizeof buf, fp)) {
-	int len = strlen (buf);
-
-	if (len < 1) continue;
-	if (buf[len - 1] == '\n') {
-	    buf[--len] = '\0';
-	    if (len == 0) continue;
-	}
-	if (!incont) {
-	    /*
-	     * check for 
-	     *
-	     *     [*.]
-	     *     class[*.]
-	     *     instance[*.]
-	     *
-	     * and set printit
-	     */
-#define match(l,n) (l > 0 && len > l && strncmp (buf, n, l) == 0 && \
-		    (buf[l] == '*' || buf[l] == '.'))
-	    printit = (buf[0] == '*' || buf[0] == '.' ||
-		       match (clen, cname) || match (ilen, iname));
-	}
-	if (printit) puts (buf);
-	incont = (buf[len - 1] == '\\');
-	if (!incont) printit = False;
+    if (argc > 1 && argv[1][0] != '-') {
+	cname = argv[1];
+	if (argc > 2 && argv[2][0] != '-')
+	    iname = argv[2];
     }
 
-    fclose (fp);
+    XrmStringToClassList(cname, classes);
+    XrmStringToNameList(iname, names);
+    for (i = 0; names[i]; i++)
+	;
+    if (!i || classes[i] || !classes[i-1]) usage ();
+    argv[0] = XrmNameToString(names[0]);
+
+    toplevel = XtAppInitialize(&xtcontext, XrmClassToString(classes[0]),
+			       NULL, 0, &argc, argv, NULL, NULL, 0);
+
+    iname = NULL;
+    cname = NULL;
+    for (i = 1; i < argc; i++) {
+	if (!strcmp(argv[i], "-1"))
+	    mode = XrmEnumOneLevel;
+	else if (argv[i][0] == '-')
+	    usage();
+	else if (!cname)
+	    cname = argv[i];
+	else if (!iname)
+	    iname = argv[i];
+	else
+	    usage();
+    }
+
+    XrmQString = XrmPermStringToQuark("String");
+
+    XrmEnumerateDatabase(XtDatabase(XtDisplay(toplevel)),
+			 names, classes, mode,
+			 DumpEntry, (XPointer)stdout);
+
     exit (0);
 }
-
-
-
