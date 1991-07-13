e5f0b8f27a72e5b8ac26d8ac7d82394d8ebe4078
converse <empty>
1991-07-13 17:13:14 +0000

    add a field to TocRec to work around a bug when switching between sequences

--- xc/programs/xmh/tocintrnl.h
+++ xc/programs/xmh/tocintrnl.h
@@ -1,4 +1,4 @@
-/* $XConsortium: tocintrnl.h,v 2.15 91/07/10 19:59:18 converse Exp $ */
+/* $XConsortium: tocintrnl.h,v 2.16 91/07/13 13:27:12 converse Exp $ */
 /*
  *			  COPYRIGHT 1987
  *		   DIGITAL EQUIPMENT CORPORATION
@@ -86,6 +86,7 @@ typedef struct _TocRec {
    Boolean	haschanged;	/* Whether it's changed in the process of */
 				/* the current commit. */
    Boolean	delete_pending;	/* Is a delete folder operation pending? */
+   Boolean	force_reset;	/* temporary bug work-around for sequences */
    char		*incfile;	/* Which file to incorporate from (if any). */
    long		lastreaddate;	/* Last time we read or wrote the cache. */
    Stack	sequence_stack;	/* Stack of sequence names. */
