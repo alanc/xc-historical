e7dee299c9a6e0c11d40b8f0abb3468015d9945a
jap <empty>
1991-07-16 13:17:42 +0000

    Corrrect copyright statement as per Austin lawyers.

--- xc/programs/Xserver/hw/ibm/skyway/skyHdwr.h
+++ xc/programs/Xserver/hw/ibm/skyway/skyHdwr.h
@@ -1,3 +1,28 @@
+/*
+ * $XConsortium: skyIO.c,v 1.1 91/05/10 09:09:03 jap Exp $
+ *
+ * Copyright IBM Corporation 1987,1988,1989,1990,1991
+ *
+ * All Rights Reserved
+ *
+ * License to use, copy, modify, and distribute this software and its
+ * documentation for any purpose and without fee is hereby granted,
+ * provided that the above copyright notice appear in all copies and that
+ * both that copyright notice and this permission notice appear in
+ * supporting documentation, and that the name of IBM not be
+ * used in advertising or publicity pertaining to distribution of the
+ * software without specific, written prior permission.
+ *
+ * IBM DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING
+ * ALL IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS, AND 
+ * NONINFRINGEMENT OF THIRD PARTY RIGHTS, IN NO EVENT SHALL
+ * IBM BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR
+ * ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+ * WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION,
+ * ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
+ * SOFTWARE.
+ *
+*/
 
 /*
  * skyHdwr.h - hardware constants
