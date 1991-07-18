23f6d4c25286ce00972fa93349ace3675d6bed4f
keith <empty>
1991-07-18 22:49:46 +0000

    remove junk

--- xc/programs/xfs/config.cpp
+++ xc/programs/xfs/config.cpp
@@ -1,17 +1,12 @@
 #
 # sample font server configuration file
 #
-clone-self = off
-cache-size = 1000000
+clone-self = on
 client-limit = 10
 use-syslog = off
-#alternate-servers = hansen:1,hansen:2
-catalogue=pcf:/usr/lib/X11/fonts/misc,pcf:/usr/lib/X11/fonts/speedo,pcf:/usr/lib/X11/fonts/75dpi,pcf:/usr/lib/X11/fonts/100dpi
-#catalogue = pcf:/u14/lemke/fonts,pcf:/u14/lemke/fontserver/fonts/data/pcf,sp:/u14/lemke/fontserver/fonts/data/speedo
-#catalogue = pcf:/u14/lemke/fonts,sp:/u14/lemke/fontserver/fonts/data/speedo
-#catalogue = pcf:/u14/lemke/fontserver/fonts/data/pcf,fs:hansen:0,snf:/usr/lib/X11/fonts/misc
-error-file = /tmp/fs-errors
-
+catalogue=/usr/lib/X11/fonts/misc,/usr/lib/X11/fonts/speedo,
+	  /usr/lib/X11/fonts/75dpi,/usr/lib/X11/fonts/100dpi
+error-file = /usr/lib/X11/fs/fs-errors
 # in decipoints
 default-point-size = 120
 default-resolutions = 75,75,100,100
