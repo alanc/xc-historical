af093520aa43850660e7b95abc5c47e73f804e24
keith <empty>
1991-07-19 16:34:56 +0000

    *** empty log message ***

--- xc/programs/xfs/config.cpp
+++ xc/programs/xfs/config.cpp
@@ -2,9 +2,8 @@
 # sample font server configuration file
 #
 clone-self = on
-client-limit = 10
 use-syslog = off
-catalogue=/usr/lib/X11/fonts/misc,/usr/lib/X11/fonts/speedo,
+catalogue=/usr/lib/X11/fonts/misc,/usr/lib/X11/fonts/Speedo,
 	  /usr/lib/X11/fonts/75dpi,/usr/lib/X11/fonts/100dpi
 error-file = /usr/lib/X11/fs/fs-errors
 # in decipoints
