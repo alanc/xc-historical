d73db4e3fe8135a4156d22641e8c1eaee26abdb6
rws <empty>
1991-07-20 15:01:54 +0000

    turn into cpp file

--- xc/programs/xfs/config.cpp
+++ xc/programs/xfs/config.cpp
@@ -1,11 +1,9 @@
-#
-# sample font server configuration file
-#
+/**/# font server configuration file
+
 clone-self = on
 use-syslog = off
-catalogue=/usr/lib/X11/fonts/misc,/usr/lib/X11/fonts/Speedo,
-	  /usr/lib/X11/fonts/75dpi,/usr/lib/X11/fonts/100dpi
-error-file = /usr/lib/X11/fs/fs-errors
-# in decipoints
+catalogue = DEFAULTFONTPATH
+error-file = FSERRORS
+/**/# in decipoints
 default-point-size = 120
 default-resolutions = 75,75,100,100
