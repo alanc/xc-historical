3fc98e8e3d89d5a0de2266727d23a47a8e5652c1
converse <empty>
1991-07-21 20:35:42 +0000

    more work on setting XtNtransientFor.  In this case, we could realize the parent earlier without any problem, so I removed special case code.

--- xc/unsupported/programs/xman/misc.c
+++ xc/unsupported/programs/xman/misc.c
@@ -1,7 +1,7 @@
 /*
  * xman - X window system manual page display program.
  *
- * $XConsortium: misc.c,v 1.25 91/06/03 17:00:11 dave Exp $
+ * $XConsortium: misc.c,v 1.26 91/06/05 14:50:24 dave Exp $
  *
  * Copyright 1987, 1988 Massachusetts Institute of Technology
  *
@@ -367,10 +367,6 @@ char * entry;
 	  if ( access(catdir, W_OK) != 0 )
 	    unlink(man_globals->tmpfile);
 	  else {
-	    if (!XtIsRealized(man_globals->save))  {
-	      XtRealizeWidget(man_globals->save);
-	      AddCursor(man_globals->save, resources.cursors.top);
-	    }
 	    x = (Position) Width(man_globals->manpagewidgets.manpage)/2;
 	    y = (Position) Height(man_globals->manpagewidgets.manpage)/2;
 	    XtTranslateCoords(manpage, x, y, &x, &y);
