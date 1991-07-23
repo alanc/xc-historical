672a6076a2489ebace52f526ad5ddf8e8301ae14
rws <empty>
1991-07-23 10:47:38 +0000

    lint

--- xc/unsupported/programs/xgas/dynamics.c
+++ xc/unsupported/programs/xgas/dynamics.c
@@ -218,7 +218,7 @@ static double hit( mol, wall)
 	else return -1.0;
     }
     else {
-	error(" In hit, illegal wall type.", deltaT);
+	error(" In hit, illegal wall type.", 0);
 	return -1.0;
     }
 }
