19a6c00e84f6f01696aace566a72a8042339f1f7
converse <empty>
1991-07-26 21:59:31 +0000

    update resource doc

--- xc/lib/Xaw/Scrollbar.h
+++ xc/lib/Xaw/Scrollbar.h
@@ -1,6 +1,4 @@
-/*
-* $XConsortium: Scrollbar.h,v 1.5 90/12/01 13:02:50 rws Exp $
-*/
+/* $XConsortium: Scrollbar.h,v 1.6 91/02/17 13:17:57 rws Exp $ */
 
 
 /***********************************************************
@@ -39,32 +37,47 @@ SOFTWARE.
 #include <X11/Xmu/Converters.h>
 #include <X11/Xfuncproto.h>
 
-/* Parameters:
+/* Scrollbar resources:
 
  Name		     Class		RepType		Default Value
  ----		     -----		-------		-------------
- background	     Background		Pixel		White
- border		     BorderColor	Pixel		Black
+ accelerators	     Accelerators	AcceleratorTable NULL
+ ancestorSensitive   AncestorSensitive	Boolean		True
+ background	     Background		Pixel		XtDefaultBackground
+ backgroundPixmap    Pixmap		Pixmap		XtUnspecifiedPixmap
+ borderColor	     BorderColor	Pixel		XtDefaultForeground
+ borderPixmap	     Pixmap		Pixmap		XtUnspecifiedPixmap
  borderWidth	     BorderWidth	Dimension	1
- destroyCallback     Callback		Function		NULL
- foreground	     Color		Pixel		Black
+ colormap	     Colormap		Colormap	parent's colormap
+ cursor		     Cursor		Cursor		None
+ cursorName	     Cursor		String		NULL
+ depth		     Depth		int		parent's depth
+ destroyCallback     Callback		XtCallbackList	NULL
+ foreground	     Foreground		Pixel		XtDefaultForeground
  height		     Height		Dimension	length or thickness
- jumpProc	     Callback		Function	NULL
+ insensitiveBorder   Insensitive	Pixmap		GreyPixmap
+ jumpProc	     Callback		XtCallbackList	NULL
  length		     Length		Dimension	1
  mappedWhenManaged   MappedWhenManaged	Boolean		True
+ minimumThumb	     MinimumThumb	Dimension	7
  orientation	     Orientation	XtOrientation	XtorientVertical
+ pointerColor	     Foreground		Pixel		XtDefaultForeground
+ pointerColorBackground Background	Pixel		XtDefaultBackground
+ screen		     Screen		Screen		parent's screen
  scrollDCursor	     Cursor		Cursor		XC_sb_down_arrow
  scrollHCursor	     Cursor		Cursor		XC_sb_h_double_arrow
  scrollLCursor	     Cursor		Cursor		XC_sb_left_arrow
- scrollProc	     Callback		Function	NULL
+ scrollProc	     Callback		XtCallbackList	NULL
  scrollRCursor	     Cursor		Cursor		XC_sb_right_arrow
  scrollUCursor	     Cursor		Cursor		XC_sb_up_arrow
  scrollVCursor	     Cursor		Cursor		XC_sb_v_double_arrow
  sensitive	     Sensitive		Boolean		True
- shown		     Shown		float		0.0
+ shown		     Shown		Float		0.0
  thickness	     Thickness		Dimension	14
- thumb		     Thumb		Pixmap		Grey
- topOfThumb	     TopOfThumb		float		0.0
+ thumb		     Thumb		Bitmap		GreyPixmap
+ thumbProc	     Callback		XtCallbackList	NULL
+ topOfThumb	     TopOfThumb		Float		0.0
+ translations	     Translations	TranslationTable see source or doc
  width		     Width		Dimension	thickness or length
  x		     Position		Position	0
  y		     Position		Position	0
