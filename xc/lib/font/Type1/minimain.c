0058bd1c92796ff166a9818fa4d7fa172ad0d119
rws <empty>
1991-10-09 19:25:51 +0000

    Initial revision

---
+++ xc/lib/font/Type1/minimain.c
@@ -0,0 +1,19 @@
+ 
+#include "ximager5.h"
+ 
+main()
+{
+       XYspace S;
+       path p;
+ 
+       InitImager();
+       S = Scale(IDENTITY, 300.0, -300.0);
+       p = Join(Line(Loc(S, 0.0, 1.0)), Line(Loc(S, 1.0, 0.0)));
+       Interior(ClosePath(p), EVENODDRULE);
+}
+ 
+void Trace()
+{
+}
+ 
+void *DEFAULTDEVICE;
