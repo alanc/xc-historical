df76bc7f56e96ca8faa44908a6f038b4c7723271
keith <empty>
1992-04-08 10:35:25 +0000

    make it work with large frames

--- xc/util/memleak/getretmips.c
+++ xc/util/memleak/getretmips.c
@@ -7,8 +7,35 @@
 /* addiu $sp, $sp, const is 001 001 11101 11101 const */
 /*                          0010 0111 1011 1101 const */
 
-#define ADJUST_STACKP		0x27bd0000
-#define ADJUST_STACKP_MASK	0xffff0000
+#define ADJUST_STACKP_C		0x27bd0000
+#define ADJUST_STACKP_C_MASK	0xffff0000
+
+/* addu $sp, $sp, $at is 000 000 11101 00001 11101 00000 100 001  */
+/*                       0000 0011 1010 0001 1110 1000 0010 0001 */
+
+#define ADJUST_STACKP_V		0x03a1e821
+#define ADJUST_STACKP_V_MASK	0xffffffff
+
+/* lui $at, const is 001 111 00000 00001 const */
+/*		     0011 1100 0000 0001 const */
+
+#define SET_UPPER_C		0x3c010000
+#define SET_UPPER_C_MASK	0xffff0000
+
+/* ori $at, $at, const is 001 101 00001 00001 const */
+/*                        0011 0100 0010 0001 const */
+
+#define OR_LOWER_C		0x34210000
+#define OR_LOWER_C_MASK		0xffff0000
+
+/* ori $at, $zero, const is 001 101 00000 00001 const */
+/*                          0011 0100 0000 0001 const */
+
+#define SET_LOWER_C		0x34010000
+#define SET_LOWER_C_MASK	0xffff0000
+
+/* jr $ra */
+#define RETURN	    0x03e00008
 
 #define CALL(f)	    (0x0c000000 | (((int) (f)) >> 2))
 
@@ -16,8 +43,8 @@
 
 typedef struct _returnCache {
     unsigned long   *returnAddress;
-    short	    raOffset;
-    short	    spAdjust;
+    long	    raOffset;
+    long	    spAdjust;
 } ReturnCacheRec, *ReturnCachePtr;
 
 static ReturnCacheRec	returnCache[HASH_SIZE];
@@ -34,13 +61,16 @@ getStackTrace (results, max)
 {
     extern unsigned long    *getReturnAddress (), *getStackPointer ();
     extern int	    main ();
-    unsigned long   *ra;
+    unsigned long   *ra, *ra_limit;
     unsigned long   *sp;
     unsigned long   inst;
     unsigned long   mainCall;
-    short	    ra_offset;
-    short	    sp_adjust;
+    unsigned short  const_upper;
+    unsigned short  const_lower;
+    long	    ra_offset;
+    long	    sp_adjust;
     Bool	    found_ra_offset, found_sp_adjust;
+    Bool	    found_const_upper, found_const_lower;
     ReturnCachePtr  rc;
 
     ra = getReturnAddress ();
@@ -52,8 +82,15 @@ getStackTrace (results, max)
 	{
 	    found_ra_offset = FALSE;
 	    found_sp_adjust = FALSE;
+	    found_const_upper = FALSE;
+	    found_const_lower = FALSE;
+	    const_upper = 0;
+	    const_lower = 0;
 	    rc->returnAddress = ra;
-	    while (!found_ra_offset || !found_sp_adjust)
+	    ra_limit = (unsigned long *) 0x10000000;
+	    ra_offset = 0;
+	    sp_adjust = -1;
+	    while ((!found_ra_offset || !found_sp_adjust) && ra < ra_limit)
 	    {
 	    	inst = *ra;
 	    	if ((inst & RESTORE_RETURNVAL_MASK) == RESTORE_RETURNVAL)
@@ -61,15 +98,46 @@ getStackTrace (results, max)
 	    	    ra_offset = inst & ~RESTORE_RETURNVAL_MASK;
 		    found_ra_offset = TRUE;
 	    	}
-	    	if ((inst & ADJUST_STACKP_MASK) == ADJUST_STACKP)
+	    	else if ((inst & ADJUST_STACKP_C_MASK) == ADJUST_STACKP_C)
 	    	{
-	    	    sp_adjust = inst & ~ADJUST_STACKP_MASK;
+	    	    sp_adjust = inst & ~ADJUST_STACKP_C_MASK;
 		    found_sp_adjust = TRUE;
 	    	}
+		else if ((inst & ADJUST_STACKP_V_MASK) == ADJUST_STACKP_V)
+		{
+		    sp_adjust = 0;
+		    found_sp_adjust = TRUE;
+		}
+		else if ((inst & SET_UPPER_C_MASK) == SET_UPPER_C)
+		{
+		    const_upper = inst & ~SET_UPPER_C_MASK;
+		    const_lower = 0;
+		    found_const_upper = TRUE;
+		}
+		else if ((inst & OR_LOWER_C_MASK) == OR_LOWER_C)
+		{
+		    const_lower = inst & ~OR_LOWER_C_MASK;
+		    found_const_lower = TRUE;
+		}
+		else if ((inst & SET_LOWER_C_MASK) == SET_LOWER_C)
+		{
+		    const_lower = inst & ~SET_LOWER_C_MASK;
+		    const_upper = 0;
+		    found_const_lower = TRUE;
+		}
+		else if (inst == RETURN)
+		    ra_limit = ra + 2;
 	    	ra++;
 	    }
+	    if (sp_adjust == 0 && (found_const_upper || found_const_lower))
+		sp_adjust = (const_upper << 16) | const_lower;
 	    rc->raOffset = ra_offset;
 	    rc->spAdjust = sp_adjust;
+	}
+	if (rc->spAdjust <= 0) 
+	{
+	    *results++ = 0;
+	    break;
 	}
 	ra = (unsigned long *) sp[rc->raOffset>>2];
 	sp += rc->spAdjust >> 2;
