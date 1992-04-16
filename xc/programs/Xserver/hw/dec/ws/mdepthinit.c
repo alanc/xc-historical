89a576ca346f3759409de55b34492f317dc412f3
rws <empty>
1992-04-16 09:57:12 +0000

    add permission notice

--- xc/programs/Xserver/hw/dec/ws/mdepthinit.c
+++ xc/programs/Xserver/hw/dec/ws/mdepthinit.c
@@ -1,4 +1,20 @@
-/* $XConsortium: mdepthinit.c,v 5.19 91/06/28 12:40:07 keith Exp $ */
+/* $XConsortium: mdepthinit.c,v 1.1 92/04/16 09:53:27 rws Exp $ */
+
+/*
+
+Copyright 1992 by the Massachusetts Institute of Technology
+
+Permission to use, copy, modify, distribute, and sell this software and its
+documentation for any purpose is hereby granted without fee, provided that
+the above copyright notice appear in all copies and that both that
+copyright notice and this permission notice appear in supporting
+documentation, and that the name of M.I.T. not be used in advertising or
+publicity pertaining to distribution of the software without specific,
+written prior permission.  M.I.T. makes no representations about the
+suitability of this software for any purpose.  It is provided "as is"
+without express or implied warranty.
+
+*/
 
 #include "X.h"
 #include "Xmd.h"
