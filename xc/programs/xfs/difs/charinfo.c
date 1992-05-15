3b0a1a5bcc9a97a95323a83e3168c4e0015a5123
gildea <empty>
1992-05-15 12:01:05 +0000

    fix copyright notice

--- xc/programs/xfs/difs/charinfo.c
+++ xc/programs/xfs/difs/charinfo.c
@@ -1,30 +1,31 @@
-/* $XConsortium: charinfo.c,v 1.3 92/05/12 18:08:13 gildea Exp $ */
+/* $XConsortium: charinfo.c,v 1.4 92/05/13 17:53:34 keith Exp $ */
 /*
  * Copyright 1990, 1991 Network Computing Devices;
  * Portions Copyright 1987 by Digital Equipment Corporation and the
  * Massachusetts Institute of Technology
  *
- * Permission to use, copy, modify, and distribute this protoype software
- * and its documentation to Members and Affiliates of the MIT X Consortium
- * any purpose and without fee is hereby granted, provided
+ * Permission to use, copy, modify, and distribute this software and its
+ * documentation for any purpose and without fee is hereby granted, provided
  * that the above copyright notice appear in all copies and that both that
  * copyright notice and this permission notice appear in supporting
- * documentation, and that the names of Network Computing Devices, Digital or
- * MIT not be used in advertising or publicity pertaining to distribution of
- * the software without specific, written prior permission.
+ * documentation, and that the names of Network Computing Devices, Digital
+ * or MIT not be used in advertising or publicity pertaining to distribution
+ * of the software without specific, written prior permission.
  *
  * NETWORK COMPUTING DEVICES, DIGITAL AND MIT DISCLAIM ALL WARRANTIES WITH
- * REGARD TO THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
- * AND FITNESS, IN NO EVENT SHALL NETWORK COMPUTING DEVICES, DIGITAL OR MIT BE
- * LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
- * OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
- * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ * REGARD TO THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS, IN NO EVENT SHALL NETWORK COMPUTING DEVICES,
+ * DIGITAL OR MIT BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL
+ * DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
+ * PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS
+ * ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
+ * THIS SOFTWARE.
  */
 /*
- * Defines the routines GetExtents and GetBitmaps,
+ * Defines GetExtents() and GetBitmaps(), which are
  * called from routines in fontinfo.c.
- * Was once on the other side of the font library interface as util/fsfuncs.c.
+ * This file was once on the other side of
+ * the font library interface as util/fsfuncs.c.
  */
 
 #include <X11/Xos.h>
