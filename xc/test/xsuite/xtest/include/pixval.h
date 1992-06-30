cbf3fbade5c36a32d85102660c19e7aa2ba7dcea
rws <empty>
1992-06-30 17:59:12 +0000

    add copyright

--- xc/test/xsuite/xtest/include/pixval.h
+++ xc/test/xsuite/xtest/include/pixval.h
@@ -1,8 +1,23 @@
+/*
+ * Copyright 1990, 1991, 1992 by the Massachusetts Institute of Technology and
+ * UniSoft Group Limited.
+ * 
+ * Permission to use, copy, modify, distribute, and sell this software and
+ * its documentation for any purpose is hereby granted without fee,
+ * provided that the above copyright notice appear in all copies and that
+ * both that copyright notice and this permission notice appear in
+ * supporting documentation, and that the names of MIT and UniSoft not be
+ * used in advertising or publicity pertaining to distribution of the
+ * software without specific, written prior permission.  MIT and UniSoft
+ * make no representations about the suitability of this software for any
+ * purpose.  It is provided "as is" without express or implied warranty.
+ *
+ * $XConsortium: pixval.h,v 1.14 92/06/11 15:31:54 rws Exp $
+ */
 #ifndef _PIX_VAL_
 #define _PIX_VAL_
 /*************
 *  Functions to mask during pixval generation runs
-* $XConsortium$
 **************/
 
 #ifdef GENERATE_PIXMAPS
