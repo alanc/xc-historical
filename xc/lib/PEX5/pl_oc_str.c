9a4c1eeec273ee4ceb127eca0f1701a544adc738
mor <empty>
1992-07-16 11:21:44 +0000

    Fixed copyright; Added PEXSyncHandle for PEXGSE.

--- xc/lib/PEX5/pl_oc_str.c
+++ xc/lib/PEX5/pl_oc_str.c
@@ -1,19 +1,20 @@
-/* $XConsortium: pl_oc_struct.c,v 1.2 92/05/26 16:20:27 mor Exp $ */
+/* $XConsortium: pl_oc_struct.c,v 1.3 92/06/12 10:50:14 mor Exp $ */
 
-/************************************************************************
-Copyright 1987,1991,1992 by Digital Equipment Corporation, Maynard,
-Massachusetts, and the Massachusetts Institute of Technology,
-Cambridge, Massachusetts.
+/******************************************************************************
+Copyright 1987,1991 by Digital Equipment Corporation, Maynard, Massachusetts
+Copyright 1992 by the Massachusetts Institute of Technology
 
                         All Rights Reserved
 
-Permission to use, copy, modify, and distribute this software and its 
-documentation for any purpose and without fee is hereby granted, 
-provided that the above copyright notice appear in all copies and that
-both that copyright notice and this permission notice appear in 
-supporting documentation, and that the names of Digital or MIT not be
-used in advertising or publicity pertaining to distribution of the
-software without specific, written prior permission.  
+Permission to use, copy, modify, distribute, and sell this software and its
+documentation for any purpose is hereby granted without fee, provided that
+the above copyright notice appear in all copies and that both that
+copyright notice and this permission notice appear in supporting
+documentation, and that the name of M.I.T. not be used in advertising or
+publicity pertaining to distribution of the software without specific,
+written prior permission.  M.I.T. makes no representations about the
+suitability of this software for any purpose.  It is provided "as is"
+without express or implied warranty.
 
 DIGITAL DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING
 ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO EVENT SHALL
@@ -22,8 +23,7 @@ ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
 WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION,
 ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
 SOFTWARE.
-
-*************************************************************************/
+******************************************************************************/
 
 #include "PEXlib.h"
 #include "PEXlibint.h"
@@ -127,4 +127,5 @@ INPUT char		*data;
     _PEXCopyPaddedBytesToOC (display, length, (char *) data);
 
     PEXFinishOC (display);
+    PEXSyncHandle (display);
 }
