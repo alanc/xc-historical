740bdc7c362858b1dd494b9dde477e4e984588a1
rws <empty>
1993-08-25 19:19:54 +0000

    Initial revision

---
+++ xc/include/Xthreads.h
@@ -0,0 +1,119 @@
+/*
+ * $XConsortium: Xthreads.h,v 1.7 93/08/22 18:20:00 rws Exp $
+ *
+ * Copyright 1993 Massachusetts Institute of Technology
+ *
+ * Permission to use, copy, modify, and distribute this software and its
+ * documentation for any purpose and without fee is hereby granted, provided
+ * that the above copyright notice appear in all copies and that both that
+ * copyright notice and this permission notice appear in supporting
+ * documentation, and that the name of M.I.T. not be used in advertising
+ * or publicity pertaining to distribution of the software without specific,
+ * written prior permission.  M.I.T. makes no representations about the
+ * suitability of this software for any purpose.  It is provided "as is"
+ * without express or implied warranty.
+ *
+ * M.I.T. DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING ALL
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO EVENT SHALL M.I.T.
+ * BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
+ * OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN 
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ */
+
+#ifndef _XTHREADS_H_
+#define _XTHREADS_H_
+
+/* Redefine these to XtMalloc/XtFree or whatever you want before including
+ * this header file.
+ */
+#ifndef xmalloc
+#define xmalloc malloc
+#endif
+#ifndef xfree
+#define xfree free
+#endif
+
+#ifdef CTHREADS
+#include <cthreads.h>
+typedef cthread_t xthread_t;
+typedef condition_t xcondition_t;
+typedef mutex_t xmutex_t;
+#define xthread_init() cthread_init()
+#define xthread_self() cthread_self()
+#define xthread_fork(func,closure) cthread_fork(func,closure)
+#define xmutex_malloc() (xmutex_t)xmalloc(sizeof(struct mutex))
+#define xmutex_init(m) mutex_init(m)
+#define xmutex_clear(m) mutex_clear(m)
+#define xmutex_lock(m) mutex_lock(m)
+#define xmutex_unlock(m) mutex_unlock(m)
+#define xcondition_malloc() (xcondition_t)xmalloc(sizeof(struct condition))
+#define xcondition_init(cv) condition_init(cv)
+#define xcondition_clear(cv) condition_clear(cv)
+#define xcondition_wait(cv,m) condition_wait(cv,m)
+#define xcondition_signal(cv) condition_signal(cv)
+#define xcondition_broadcast(cv) condition_broadcast(cv)
+#else
+#ifdef sun
+#include <thread.h>
+typedef thread_t xthread_t;
+typedef cond_t *xcondition_t;
+typedef mutex_t *xmutex_t;
+#define xthread_self() thr_self()
+#define xthread_fork(func,closure) thr_create(NULL,0,func,closure,THR_DETACHED,NULL);
+#define xmutex_malloc() (xmutex_t)xmalloc(sizeof(mutex_t))
+#define xmutex_init(m) mutex_init(m,USYNC_THREAD,0)
+#define xmutex_clear(m) mutex_destroy(m)
+#define xmutex_lock(m) mutex_lock(m)
+#define xmutex_unlock(m) mutex_unlock(m)
+#define xcondition_malloc() (xcondition_t)xmalloc(sizeof(cond_t))
+#define xcondition_init(cv) cond_init(cv,USYNC_THREAD,0)
+#define xcondition_clear(cv) cond_destroy(cv)
+#define xcondition_wait(cv,m) cond_wait(cv,m)
+#define xcondition_signal(cv) cond_signal(cv)
+#define xcondition_broadcast(cv) cond_broadcast(cv)
+#else
+#include <pthread.h>
+typedef pthread_t xthread_t;
+typedef pthread_cond_t *xcondition_t;
+typedef pthread_mutex_t *xmutex_t;
+#define xthread_self() pthread_self()
+#define xthread_fork(func,closure) { pthread_t _tmpxthr; \
+	pthread_create(&_tmpxthr,pthread_attr_default,func,closure); }
+#define xmutex_malloc() (xmutex_t)xmalloc(sizeof(pthread_mutex_t))
+#define xmutex_init(m) pthread_mutex_init(m, pthread_mutexattr_default)
+#define xmutex_clear(m) pthread_mutex_destroy(m)
+#define xmutex_lock(m) pthread_mutex_lock(m)
+#define xmutex_unlock(m) pthread_mutex_unlock(m)
+#define xcondition_malloc() (xcondition_t)xmalloc(sizeof(pthread_cond_t))
+#define xcondition_init(c) pthread_cond_init(c, pthread_condattr_default)
+#define xcondition_clear(c) pthread_cond_destroy(c)
+#define xcondition_wait(c,m) pthread_cond_wait(c,m)
+#define xcondition_signal(c) pthread_cond_signal(c)
+#define xcondition_broadcast(c) pthread_cond_broadcast(c)
+#ifdef _DECTHREADS_
+static xthread_t _X_no_thread_id;
+#define xthread_have_id(id) !pthread_equal(id, _X_no_thread_id)
+#define xthread_clear_id(id) id = _X_no_thread_id
+#define xthread_equal(id1,id2) pthread_equal(id1, id2)
+#endif
+#endif
+#endif
+#ifndef xcondition_free
+#define xcondition_free(c) xfree((char *)c)
+#endif
+#ifndef xmutex_free
+#define xmutex_free(m) xfree((char *)m)
+#endif
+#ifndef xthread_have_id
+#define xthread_have_id(id) id
+#endif
+#ifndef xthread_clear_id
+#define xthread_clear_id(id) id = 0
+#endif
+#ifndef xthread_equal
+#define xthread_equal(id1,id2) id1 == id2
+#endif
+
+#endif /* _XTHREADS_H_ */
