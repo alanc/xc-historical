ea7857fbd980d24167a6f3149ad7d57f6472b5b5
dpw <empty>
1993-09-03 08:36:28 +0000

    support for SYNC priorities

--- xc/programs/Xserver/include/opaque.h
+++ xc/programs/Xserver/include/opaque.h
@@ -1,4 +1,4 @@
-/* $XConsortium: opaque.h,v 1.9 89/07/16 14:37:10 rws Exp $ */
+/* $XConsortium: opaque.h,v 1.10 93/07/12 09:44:59 dpw Exp $ */
 
 #ifndef OPAQUE_H
 #define OPAQUE_H
@@ -14,6 +14,9 @@ extern char dispatchException;
 /* bit values for dispatchException */
 #define DE_RESET     1
 #define DE_TERMINATE 2
+#ifdef SYNC
+#define DE_PRIORITYCHANGE 4  /* set when a client's priority changes */
+#endif
 
 extern long TimeOutValue;
 extern long ScreenSaverTime;
