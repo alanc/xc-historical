bd79bdeefb2e2ba043f0788f01603ef69aa9e925
rws <empty>
1993-09-12 16:36:17 +0000

    get \ processing right on NT

--- xc/programs/xrdb/xrdb.c
+++ xc/programs/xrdb/xrdb.c
@@ -1,7 +1,7 @@
 /*
  * xrdb - X resource manager database utility
  *
- * $XConsortium: xrdb.c,v 11.69 93/09/11 13:58:54 rws Exp $
+ * $XConsortium: xrdb.c,v 11.70 93/09/12 15:31:46 rws Exp $
  */
 
 /*
@@ -352,11 +352,21 @@ void ReadFile(buffer, input)
 	register Buffer *buffer;
 	FILE *input;
 {
-	     char	buf[BUFSIZ];
+	     char	buf[BUFSIZ + 1];
     register int	bytes;
 
     buffer->used = 0;
     while (!feof(input) && (bytes = fread(buf, 1, BUFSIZ, input)) > 0) {
+#ifdef WIN32
+	char *p;
+	buf[bytes] = '\0';
+	for (p = buf; p = strchr(p, '\r'); ) {
+	    if (p[-1] == '\\' && p[1] == '\n') {
+		bytes -= 3;
+		strcpy(p - 1, p + 2);
+	    }
+	}
+#endif
 	AppendToBuffer(buffer, buf, bytes);
     }
     AppendToBuffer(buffer, "", 1);
