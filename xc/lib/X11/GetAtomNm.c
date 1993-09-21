8c99415a930682d939c901fb4c61ef08cc35d43d
rws <empty>
1993-09-21 22:58:54 +0000

    XGetAtomNames

--- xc/lib/X11/GetAtomNm.c
+++ xc/lib/X11/GetAtomNm.c
@@ -1,4 +1,4 @@
-/* $XConsortium: XGetAtomNm.c,v 11.17 93/08/31 19:26:30 rws Exp $ */
+/* $XConsortium: GetAtomNm.c,v 11.18 93/09/01 20:39:02 rws Exp $ */
 /* Copyright    Massachusetts Institute of Technology    1986	*/
 
 /*
@@ -33,79 +33,144 @@ typedef struct _XDisplayAtoms {
     Entry table[TABLESIZE];
 } AtomTable;
 
-#define HASH(sig) ((sig) & (TABLESIZE-1))
-#define REHASHVAL(sig) ((((sig) % (TABLESIZE-3)) + 2) | 1)
-#define REHASH(idx,rehash) ((idx + rehash) & (TABLESIZE-1))
-
-char *XGetAtomName(dpy, atom)
-register Display *dpy;
-Atom atom;
+static
+char *_XGetAtomName(dpy, atom)
+    Display *dpy;
+    Atom atom;
 {
-    xGetAtomNameReply rep;
     xResourceReq *req;
-    char *storage;
+    char *name;
     register Entry *table;
     register int idx;
-    int firstidx, rehash;
     register Entry e;
-    register char *s1, c;
-    register unsigned long sig;
 
-    LockDisplay(dpy);
-    /* look in the cache first */
     if (dpy->atoms) {
 	table = dpy->atoms->table;
 	for (idx = TABLESIZE; --idx >= 0; ) {
 	    if ((e = *table++) && (e->atom == atom)) {
 		idx = strlen(EntryName(e)) + 1;
-		if (storage = (char *)Xmalloc(idx))
-		    strcpy(storage, EntryName(e));
-		UnlockDisplay(dpy);
-		return storage;
+		if (name = (char *)Xmalloc(idx))
+		    strcpy(name, EntryName(e));
+		return name;
 	    }		
 	}
     }
     GetResReq(GetAtomName, atom, req);
+    return (char *)NULL;
+}
+
+char *XGetAtomName(dpy, atom)
+    register Display *dpy;
+    Atom atom;
+{
+    xGetAtomNameReply rep;
+    char *name;
+
+    LockDisplay(dpy);
+    if (name = _XGetAtomName(dpy, atom)) {
+	UnlockDisplay(dpy);
+	return name;
+    }	
     if (_XReply(dpy, (xReply *)&rep, 0, xFalse) == 0) {
 	UnlockDisplay(dpy);
 	SyncHandle();
 	return(NULL);
     }
-    if (storage = (char *) Xmalloc(rep.nameLength+1)) {
-	_XReadPad(dpy, storage, (long)rep.nameLength);
-	storage[rep.nameLength] = '\0';
-	/* store it in the cache */
-	if (!dpy->atoms) {
-	    dpy->atoms = (AtomTable *)Xcalloc(1, sizeof(AtomTable));
-	    dpy->free_funcs->atoms = _XFreeAtomTable;
-	}
-	if (dpy->atoms) {
-	    table = dpy->atoms->table;
-	    sig = 0;
-	    for (s1 = storage; c = *s1++; )
-		sig = (sig << 1) + c;
-	    firstidx = idx = HASH(sig);
-	    if (table[idx]) {
-		rehash = REHASHVAL(sig);
-		do
-		    idx = REHASH(idx, rehash);
-		while (idx != firstidx && table[idx]);
-	    }
-	    e = (Entry)Xmalloc(sizeof(EntryRec) + rep.nameLength + 1);
-	    if (e) {
-		e->sig = sig;
-		e->atom = atom;
-		strcpy(EntryName(e), storage);
-		if (table[idx])
-		    Xfree((char *)table[idx]);
-		table[idx] = e;
-	    }
-	}
+    if (name = (char *) Xmalloc(rep.nameLength+1)) {
+	_XReadPad(dpy, name, (long)rep.nameLength);
+	name[rep.nameLength] = '\0';
+	_XUpdateAtomCache(dpy, name, atom, 0, -1, 0);
     } else {
 	_XEatData(dpy, (unsigned long) (rep.nameLength + 3) & ~3);
-	storage = (char *) NULL;
+	name = (char *) NULL;
     }
     UnlockDisplay(dpy);
     SyncHandle();
-    return(storage);
+    return(name);
+}
+
+typedef struct {
+    unsigned long start_seq;
+    Atom *atoms;
+    char **names;
+    int idx;
+    int count;
+} _XGetAtomNameState;
+
+static
+Bool _XGetAtomNameHandler(dpy, rep, buf, len, data)
+    register Display *dpy;
+    register xReply *rep;
+    char *buf;
+    int len;
+    XPointer data;
+{
+    register _XGetAtomNameState *state;
+    xGetAtomNameReply replbuf;
+    register xGetAtomNameReply *repl;
+
+    state = (_XGetAtomNameState *)data;
+    if (dpy->last_request_read < state->start_seq)
+	return False;
+    while (state->idx < state->count && state->names[state->idx])
+	state->idx++;
+    if (state->idx >= state->count)
+	return False;
+    if (rep->generic.type == X_Error)
+	return False;
+    repl = (xGetAtomNameReply *)
+	_XGetAsyncReply(dpy, (char *)&replbuf, rep, buf, len,
+			(SIZEOF(xGetAtomNameReply) - SIZEOF(xReply)) >> 2,
+			False);
+    state->names[state->idx] = (char *) Xmalloc(repl->nameLength+1);
+    _XGetAsyncData(dpy, state->names[state->idx], buf, len,
+		   SIZEOF(xGetAtomNameReply), repl->nameLength,
+		   repl->length << 2);
+    _XUpdateAtomCache(dpy, state->names[state->idx],
+		      state->atoms[state->idx], 0, -1, 0);
+    return True;
+}
+
+void
+XGetAtomNames (dpy, atoms, count, names_return)
+    Display *dpy;
+    Atom *atoms;
+    int count;
+    char **names_return;
+{
+    _XAsyncHandler async;
+    _XGetAtomNameState async_state;
+    xGetAtomNameReply rep;
+    int i;
+    int missed = -1;
+
+    LockDisplay(dpy);
+    async_state.start_seq = dpy->request + 1;
+    async_state.atoms = atoms;
+    async_state.names = names_return;
+    async_state.idx = 0;
+    async_state.count = count - 1;
+    async.next = dpy->async_handlers;
+    async.handler = _XGetAtomNameHandler;
+    async.data = (XPointer)&async_state;
+    dpy->async_handlers = &async;
+    for (i = 0; i < count; i++) {
+	if (!(names_return[i] = _XGetAtomName(dpy, atoms[i])))
+	    missed = i;
+    }
+    if (missed >= 0) {
+	if (_XReply(dpy, (xReply *)&rep, 0, xFalse)) {
+	    if (names_return[missed] = (char *) Xmalloc(rep.nameLength+1)) {
+		_XReadPad(dpy, names_return[missed], (long)rep.nameLength);
+		names_return[missed][rep.nameLength] = '\0';
+		_XUpdateAtomCache(dpy, names_return[missed], atoms[missed],
+				  0, -1, 0);
+	    } else
+		_XEatData(dpy, (unsigned long) (rep.nameLength + 3) & ~3);
+	}
+    }
+    DeqAsyncHandler(dpy, &async);
+    UnlockDisplay(dpy);
+    if (missed >= 0)
+	SyncHandle();
 }
