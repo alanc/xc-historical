a3299b11de76628e35670e4c1a37080c6f58ef4e
kaleb <empty>
1993-10-06 16:56:53 +0000

    add Sun copyright

--- xc/lib/Xt/ClickTime.c
+++ xc/lib/Xt/ClickTime.c
@@ -1,8 +1,9 @@
-/* $XConsortium: ClickTime.c,v 1.2 91/01/06 13:32:02 rws Exp $ */
+/* $XConsortium: ClickTime.c,v 1.3 93/08/27 16:31:01 kaleb Exp $ */
 
 /***********************************************************
 Copyright 1987, 1988 by Digital Equipment Corporation, Maynard, Massachusetts,
 and the Massachusetts Institute of Technology, Cambridge, Massachusetts.
+Copyright 1993 by Sun Microsystems, Inc. Mountain View, CA
 
                         All Rights Reserved
 
@@ -10,7 +11,7 @@ Permission to use, copy, modify, and distribute this software and its
 documentation for any purpose and without fee is hereby granted, 
 provided that the above copyright notice appear in all copies and that
 both that copyright notice and this permission notice appear in 
-supporting documentation, and that the names of Digital or MIT not be
+supporting documentation, and that the names of Digital,MIT, or Sun not be
 used in advertising or publicity pertaining to distribution of the
 software without specific, written prior permission.  
 
@@ -21,6 +22,15 @@ ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
 WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION,
 ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
 SOFTWARE.
+
+SUN DISCLAIMS ALL WARRANTIES WITH REGARD TO  THIS  SOFTWARE,
+INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FIT-
+NESS FOR A PARTICULAR PURPOSE. IN NO EVENT SHALL SUN BE  LI-
+ABLE  FOR  ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR
+ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE,  DATA  OR
+PROFITS,  WHETHER  IN  AN  ACTION OF CONTRACT, NEGLIGENCE OR
+OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION  WITH
+THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 ******************************************************************/
 
