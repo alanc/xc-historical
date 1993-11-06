aaa2a4b38f19b5e50f794b26699cc52b908a1830
rws <empty>
1993-11-06 15:27:10 +0000

    lint

--- xc/programs/Xserver/XIE/mixie/export/mephoto.c
+++ xc/programs/Xserver/XIE/mixie/export/mephoto.c
@@ -1,4 +1,4 @@
-/* $XConsortium: mephoto.c,v 1.1 93/10/26 09:49:11 rws Exp $ */
+/* $XConsortium: mephoto.c,v 1.2 93/10/26 14:13:46 rws Exp $ */
 /**** module mephoto.c ****/
 /******************************************************************************
 				NOTICE
@@ -504,7 +504,7 @@ static int InitializeEPhotoUncomByPixel(flo,ped)
 	pvt->shift     = 0; 
       }
     } else if (depth1 + depth2 + depth3 <= 8) {
-      CARD8 ones = ~0,smask1,smask2,smask3,shift1,shift2,shift3;
+      CARD8 ones = 0xff,smask1,smask2,smask3,shift1,shift2,shift3;
       if (tec->fillOrder == xieValMSFirst) {
 	smask1 = ~(ones>>depth1);
 	smask2 = ~(ones>>(depth1 + depth2) | smask1);
