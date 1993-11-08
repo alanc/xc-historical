b409c8d44e175f9155317d2fee2e537739717c94
mor <empty>
1993-11-08 11:29:05 +0000

    added SmsClientHostName

--- xc/lib/SM/sm_manager.c
+++ xc/lib/SM/sm_manager.c
@@ -1,4 +1,4 @@
-/* $XConsortium: sm_manager.c,v 1.8 93/09/27 21:09:11 mor Exp $ */
+/* $XConsortium: sm_manager.c,v 1.9 93/09/28 10:38:00 mor Exp $ */
 /******************************************************************************
 Copyright 1993 by the Massachusetts Institute of Technology,
 
@@ -17,7 +17,56 @@ purpose.  It is provided "as is" without express or implied warranty.
 #include <X11/SM/SMlib.h>
 #include <X11/SM/SMlibint.h>
 
+#include <stdio.h>
+#include <sys/types.h>
+#ifndef WIN32
 
+#include <sys/socket.h>
+
+#ifdef UNIXCONN
+#include <sys/un.h>
+#endif
+
+#ifdef TCPCONN
+# include <sys/param.h>
+# include <netinet/in.h>
+# ifndef hpux
+#  ifdef apollo
+#   ifndef NO_TCP_H
+#    include <netinet/tcp.h>
+#   endif
+#  else
+#   include <netinet/tcp.h>
+#  endif
+# endif
+#endif
+
+#ifdef DNETCONN
+#include <netdnet/dn.h>
+#endif
+
+#else
+
+#define BOOL wBOOL
+#undef Status
+#define Status wStatus
+#include <winsock.h>
+#undef Status
+#define Status int
+#undef BOOL
+#include <X11/Xw32defs.h>
+#undef close
+#define close closesocket
+
+#endif
+
+#include <arpa/nameser.h>
+#include <resolv.h>
+
+static ns_inaddrtohostname ();
+
+
+
 Status
 SmsInitialize (vendor, release, newClientProc, managerData,
     errorLength, errorStringRet)
@@ -101,6 +150,69 @@ char 	*release;
      */
 
     (*_SmsNewClientProc) (smsConn, _SmsNewClientData, &smsConn->callbacks);
+}
+
+
+
+char *
+SmsClientHostName (smsConn)
+
+SmsConn smsConn;
+
+{
+    union {
+	struct sockaddr		sa;
+#ifdef UNIXCONN
+	struct sockaddr_un	un;
+#endif /* UNIXCONN */
+#ifdef TCPCONN
+	struct sockaddr_in	in;
+#endif /* TCPCONN */
+#ifdef DNETCONN
+	struct sockaddr_dn	dn;
+#endif /* DNETCONN */
+    } from;
+
+    int		fromlen = sizeof (from);
+    char 	addr[1024];
+    char	*hostname;
+
+    if (getpeername (IceConnectionNumber (smsConn->iceConn),
+	&from.sa, &fromlen) < 0)
+    {
+	return (NULL);
+    }
+
+    if (!fromlen)
+	strcpy (addr, "local");
+    else
+    {
+	switch (from.sa.sa_family)
+	{
+	case AF_UNSPEC:
+#ifdef UNIXCONN
+	case AF_UNIX:
+#endif
+	    strcpy (addr, "local");
+	    break;
+#ifdef TCPCONN
+	case AF_INET:
+            ns_inaddrtohostname (&from.in.sin_addr, addr, sizeof (addr));
+	    break;
+#endif
+#ifdef DNETCONN
+	case AF_DECnet:
+	    sprintf (addr, "%s", dnet_ntoa (from.dn.sdn_add));
+	    break;
+#endif
+	default:
+	    strcpy (addr, "???");
+	}
+    }
+
+    hostname = strdup (addr);
+
+    return (hostname);
 }
 
 
@@ -286,4 +398,119 @@ SmsConn smsConn;
 
 	free ((char *) smsConn);
     }
+}
+
+
+
+/*
+ * Get a host name from an Internet address.
+ * Thanks to Keith Packard for this code.
+ */
+
+#define DATA_LEN 4096
+
+struct ns_answer {
+	u_short	type;
+	u_short	class;
+	u_short	len;
+	u_char	data[DATA_LEN];
+};
+
+static
+hostorderheader (h)
+	HEADER	*h;
+{
+	h->id = ntohs(h->id);
+	h->qdcount = ntohs(h->qdcount);
+	h->ancount = ntohs(h->ancount);
+	h->nscount = ntohs(h->nscount);
+	h->arcount = ntohs(h->arcount);
+}
+
+static
+do_query (type, class, string, answers, max)
+	int			type, class;
+	char			*string;
+	struct ns_answer	*answers;
+	int			max;
+{
+	int		i;
+	int		msglen, anslen;
+	u_char		msg[PACKETSZ];
+	u_char		ans[PACKETSZ];
+	HEADER		*h;
+	u_char		*a, *eom;
+	int		len;
+
+	msglen = res_mkquery (QUERY, string, class, type,
+			   (char *) 0, 0, (char *) 0,
+			   msg, sizeof msg);
+	anslen = res_send(msg, msglen, ans, sizeof ans);
+	if (anslen < sizeof (HEADER))
+		return 0;
+	h = (HEADER *) ans;
+	hostorderheader(h);
+	a = ans + sizeof (HEADER);
+	eom = ans + anslen;
+	/* skip query stuff */
+	if (h->qdcount)
+	{
+		for (i = 0; i < h->qdcount; i++)
+		{
+			len = dn_skipname(a, eom);
+			if (len < 0)
+				return 0;
+			a += len;
+			a += 2;	/* type */
+			a += 2;	/* class */
+		}
+	}
+	for (i = 0; i < h->ancount && i < max; i++)
+	{
+		len = dn_skipname (a, eom);
+		if (len < 0)
+			return 0;
+		a += len;
+		GETSHORT (answers[i].type, a);	/* type */
+		GETSHORT (answers[i].class, a);	/* class */
+		a += 4;				/* ttl */
+		GETSHORT (len, a);		/* dlen */
+		if (len > DATA_LEN)
+			len = DATA_LEN;
+		bcopy (a, answers[i].data, len);
+		answers[i].len = len;
+	}
+	return i;
+}
+
+static
+ns_inaddrtohostname (addr,name,max)
+	struct in_addr	*addr;
+	char		*name;
+	int		max;
+{
+	struct ns_answer	answer;
+	char			addrstring[256];
+	u_char			*a;
+	int			ret;
+	int			l;
+
+	a = (u_char *) addr;
+	sprintf (addrstring, "%d.%d.%d.%d.IN-ADDR.ARPA",
+	    a[3], a[2], a[1], a[0]);
+	ret = do_query (T_PTR, C_IN, addrstring, &answer, 1);
+	if (ret <= 0)
+		return 0;
+	if (answer.type != T_PTR || answer.class != C_IN ||
+	    answer.len > max)
+		return 0;
+	a = (u_char *) answer.data;
+	while (l = *a++) {
+		while (l--)
+			*name++ = (char) *a++;
+		if (*a)
+			*name++ = '.';
+	}
+	*name = '\0';
+	return 1;
 }
