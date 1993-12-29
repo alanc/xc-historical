12bace119f35e12192815ef6f7934e6ad57c5091
rob <empty>
1993-12-29 15:13:12 +0000

    MTX merge

--- xc/workInProgress/MTXserver/mfb/mfbgc.c
+++ xc/workInProgress/MTXserver/mfb/mfbgc.c
@@ -20,8 +20,29 @@ WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION,
 ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
 SOFTWARE.
 
+Copyright 1992, 1993 Data General Corporation;
+Copyright 1992, 1993 OMRON Corporation  
+
+Permission to use, copy, modify, distribute, and sell this software and its
+documentation for any purpose is hereby granted without fee, provided that the
+above copyright notice appear in all copies and that both that copyright
+notice and this permission notice appear in supporting documentation, and that
+neither the name OMRON or DATA GENERAL be used in advertising or publicity
+pertaining to distribution of the software without specific, written prior
+permission of the party whose name is to be used.  Neither OMRON or 
+DATA GENERAL make any representation about the suitability of this software
+for any purpose.  It is provided "as is" without express or implied warranty.  
+
+OMRON AND DATA GENERAL EACH DISCLAIM ALL WARRANTIES WITH REGARD TO THIS
+SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS,
+IN NO EVENT SHALL OMRON OR DATA GENERAL BE LIABLE FOR ANY SPECIAL, INDIRECT
+OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE,
+DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
+TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE
+OF THIS SOFTWARE.
+
 ******************************************************************/
-/* $XConsortium: mfbgc.c,v 5.31 93/12/13 17:26:41 dpw Exp $ */
+/* $XConsortium: mfbgc.c,v 1.1 93/12/29 12:30:56 rob Exp $ */
 #include "X.h"
 #include "Xmd.h"
 #include "Xproto.h"
@@ -415,7 +436,11 @@ mfbCreateGC(pGC)
     pGC->funcs = &mfbFuncs;
 
     /* mfb wants to translate before scan convesion */
+#if defined(MTX) && defined(TRANSLATE_COORDS)
+    pGC->miTranslate = 0;
+#else
     pGC->miTranslate = 1;
+#endif
 
     pPriv = (mfbPrivGC *)(pGC->devPrivates[mfbGCPrivateIndex].ptr);
     pPriv->rop = mfbReduceRop(pGC->alu, pGC->fgPixel);
