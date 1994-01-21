0eacfb28f801eeb194c19c5484d20167308d9822
dpw <empty>
1994-01-21 22:12:27 +0000

    better comments

--- xc/include/XWDFile.h
+++ xc/include/XWDFile.h
@@ -1,4 +1,4 @@
-/* $XConsortium: XWDFile.h,v 1.14 91/01/06 12:18:26 rws Exp $ */
+/* $XConsortium: XWDFile.h,v 1.15 93/09/29 17:48:31 rws Exp $ */
 /* Copyright 1985, 1986, Massachusetts Institute of Technology */
 
 /*
@@ -24,27 +24,6 @@ without express or implied warranty.
  *              18-Nov-86  - version 6 for saving/restoring color maps
  */
 
-
-/*
- * This is not portable between machines of differing word sizes.  To make
- * it portable, do the following things:
- *
- *     o  #include <X11/Xmd.h>
- *     o  remove the typedef for xwdval
- *     o  replace all instances of xwdval with the appropriate CARD32 ... B32
- *     o  make sure that XWDFileHeader is padded to quadword boundaries
- *     o  make sure the window name is written out quadword aligned
- *     o  create an XWDColor structure that contains the same fields as XColor
- *        but which is defined in terms of CARD32 B32, CARD16 B16, and CARD8
- *     o  convert XColor structures to XWDColor structures in xwd
- *     o  remove all xwdval casts from xwd
- *     o  pack image data before writing out if necessary
- *     o  replace casts from xwdval objects in xwud with cvtINT macros
- *     o  convert XWDColor structures to XColor structures
- *     o  unpack data after reading in if necessary
- */
-
-
 #include <X11/Xmd.h>
 
 #define XWD_FILE_VERSION 7
@@ -53,33 +32,57 @@ without express or implied warranty.
 
 typedef CARD32 xwdval;		/* for old broken programs */
 
+/* Values in the file are most significant byte first. */
+
 typedef struct _xwd_file_header {
-	CARD32 header_size B32;  /* Size of the entire file header (bytes). */
-	CARD32 file_version B32;	/* XWD_FILE_VERSION */
-	CARD32 pixmap_format B32;	/* Pixmap format */
+	/* header_size = SIZEOF(XWDheader) + length of null-terminated
+	 * window name. */
+	CARD32 header_size B32;		
+
+	CARD32 file_version B32;	/* = XWD_FILE_VERSION above */
+	CARD32 pixmap_format B32;	/* ZPixmap or XYPixmap */
 	CARD32 pixmap_depth B32;	/* Pixmap depth */
 	CARD32 pixmap_width B32;	/* Pixmap width */
 	CARD32 pixmap_height B32;	/* Pixmap height */
-	CARD32 xoffset B32;		/* Bitmap x offset */
-	CARD32 byte_order B32;		/* MSBFirst, LSBFirst */
-	CARD32 bitmap_unit B32;		/* Bitmap unit */
-	CARD32 bitmap_bit_order B32;	/* MSBFirst, LSBFirst */
-	CARD32 bitmap_pad B32;		/* Bitmap scanline pad */
+	CARD32 xoffset B32;		/* Bitmap x offset, normally 0 */
+	CARD32 byte_order B32;		/* of image data: MSBFirst, LSBFirst */
+
+	/* bitmap_unit applies to bitmaps (depth 1 format XY) only.
+	 * It is the number of bits that each scanline is padded to. */
+	CARD32 bitmap_unit B32;		
+
+	CARD32 bitmap_bit_order B32;	/* bitmaps only: MSBFirst, LSBFirst */
+
+	/* bitmap_pad applies to pixmaps (non-bitmaps) only.
+	 * It is the number of bits that each scanline is padded to. */
+	CARD32 bitmap_pad B32;		
+
 	CARD32 bits_per_pixel B32;	/* Bits per pixel */
-	CARD32 bytes_per_line B32;	/* Bytes per scanline */
+
+	/* bytes_per_line is pixmap_width padded to bitmap_unit (bitmaps)
+	 * or bitmap_pad (pixmaps).  It is the delta (in bytes) to get
+	 * to the same x position on an adjacent row. */
+	CARD32 bytes_per_line B32;
 	CARD32 visual_class B32;	/* Class of colormap */
 	CARD32 red_mask B32;		/* Z red mask */
 	CARD32 green_mask B32;		/* Z green mask */
 	CARD32 blue_mask B32;		/* Z blue mask */
 	CARD32 bits_per_rgb B32;	/* Log2 of distinct color values */
-	CARD32 colormap_entries B32;	/* Number of entries in colormap */
-	CARD32 ncolors B32;		/* Number of Color structures */
+	CARD32 colormap_entries B32;	/* Number of entries in colormap; not used? */
+	CARD32 ncolors B32;		/* Number of XWDColor structures */
 	CARD32 window_width B32;	/* Window width */
 	CARD32 window_height B32;	/* Window height */
 	CARD32 window_x B32;		/* Window upper left X coordinate */
 	CARD32 window_y B32;		/* Window upper left Y coordinate */
 	CARD32 window_bdrwidth B32;	/* Window border width */
 } XWDFileHeader;
+
+/* Null-terminated window name follows the above structure. */
+
+/* Next comes XWDColor structures, at offset XWDFileHeader.header_size in
+ * the file.  XWDFileHeader.ncolors tells how many XWDColor structures
+ * there are.
+ */
 
 typedef struct {
         CARD32	pixel B32;
@@ -89,3 +92,5 @@ typedef struct {
         CARD8	flags;
         CARD8	pad;
 } XWDColor;
+
+/* Last comes the image data in the format described by XWDFileHeader. */
