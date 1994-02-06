c814cf6d956045380a462dd6521e90a3c3d111e7
rws <empty>
1994-02-06 16:28:50 +0000

    copy _XProcessWindowAttributes from Xlib, don't rely on internal symbol

--- xc/unsupported/extensions/screensaver/lib/Xext/XScrnSaver.c
+++ xc/unsupported/extensions/screensaver/lib/Xext/XScrnSaver.c
@@ -1,5 +1,5 @@
 /*
- * $XConsortium: XScrnSaver.c,v 1.2 92/02/28 18:10:49 keith Exp $
+ * $XConsortium: XScrnSaver.c,v 1.3 92/03/05 19:18:06 keith Exp $
  *
  * Copyright 1992 Massachusetts Institute of Technology
  *
@@ -236,6 +236,69 @@ void XScreenSaverSelectInput (dpy, drawable, mask)
     SyncHandle ();
 }
 
+static void
+XScreenSaverProcessWindowAttributes (dpy, req, valuemask, attributes)
+    register Display *dpy;
+    xChangeWindowAttributesReq *req;
+    register unsigned long valuemask;
+    register XSetWindowAttributes *attributes;
+    {
+    unsigned long values[32];
+    register unsigned long *value = values;
+    unsigned int nvalues;
+
+    if (valuemask & CWBackPixmap)
+	*value++ = attributes->background_pixmap;
+	
+    if (valuemask & CWBackPixel)
+    	*value++ = attributes->background_pixel;
+
+    if (valuemask & CWBorderPixmap)
+    	*value++ = attributes->border_pixmap;
+
+    if (valuemask & CWBorderPixel)
+    	*value++ = attributes->border_pixel;
+
+    if (valuemask & CWBitGravity)
+    	*value++ = attributes->bit_gravity;
+
+    if (valuemask & CWWinGravity)
+	*value++ = attributes->win_gravity;
+
+    if (valuemask & CWBackingStore)
+        *value++ = attributes->backing_store;
+    
+    if (valuemask & CWBackingPlanes)
+	*value++ = attributes->backing_planes;
+
+    if (valuemask & CWBackingPixel)
+    	*value++ = attributes->backing_pixel;
+
+    if (valuemask & CWOverrideRedirect)
+    	*value++ = attributes->override_redirect;
+
+    if (valuemask & CWSaveUnder)
+    	*value++ = attributes->save_under;
+
+    if (valuemask & CWEventMask)
+	*value++ = attributes->event_mask;
+
+    if (valuemask & CWDontPropagate)
+	*value++ = attributes->do_not_propagate_mask;
+
+    if (valuemask & CWColormap)
+	*value++ = attributes->colormap;
+
+    if (valuemask & CWCursor)
+	*value++ = attributes->cursor;
+
+    req->length += (nvalues = value - values);
+
+    nvalues <<= 2;			    /* watch out for macros... */
+    Data32 (dpy, (long *) values, (long)nvalues);
+
+    }
+
 void XScreenSaverSetAttributes (dpy, drawable, x, y, width, height,
 				border_width, depth, class, visual,
 				valuemask, attributes)
@@ -275,7 +338,8 @@ void XScreenSaverSetAttributes (dpy, drawable, x, y, width, height,
 	req->visualID = visual->visualid;
     /* abuse an Xlib internal interface - is this legal for us? */
     if (req->mask = valuemask) 
-        _XProcessWindowAttributes (dpy, (xChangeWindowAttributesReq *)req, 
+        XScreenSaverProcessWindowAttributes (dpy,
+			(xChangeWindowAttributesReq *)req, 
 			valuemask, attributes);
     UnlockDisplay (dpy);
     SyncHandle ();
