445f035c80ee391597a1f569fdfa1860ee331950
gildea <empty>
1994-03-28 15:59:21 +0000

    bumped to 3.1.6 for R6

--- xc/unsupported/programs/xman/version.h
+++ xc/unsupported/programs/xman/version.h
@@ -1,7 +1,7 @@
 /*
  * xman - X window system manual page display program.
  *
- * $XConsortium: version.h,v 1.8 91/02/08 10:45:20 gildea Exp $
+ * $XConsortium: version.h,v 1.9 91/06/08 18:15:37 rws Exp $
  *
  * Copyright 1987, 1988 Massachusetts Institute of Technology
  *
@@ -19,5 +19,4 @@
  * Created:   July 18, 1988 
  */
 
-#define XMAN_VERSION "Xman Version 3.1.5 - X11R5."
-
+#define XMAN_VERSION "Xman Version 3.1.6 - X11R6."
