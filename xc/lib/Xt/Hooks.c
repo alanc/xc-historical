f327a23dbb630e6ffc697fb41a05081e11b6ed1f
kaleb <empty>
1994-04-12 16:44:56 +0000

    Add MIT copyright, again!

--- xc/lib/Xt/Hooks.c
+++ xc/lib/Xt/Hooks.c
@@ -1,4 +1,18 @@
-/* $XConsortium: Hooks.c,v 1.5 94/01/08 18:50:53 kaleb Exp $ */
+/* $XConsortium: Hooks.c,v 1.6 94/01/14 17:56:13 kaleb Exp $ */
+
+/*
+ * Copyright 1993 Massachusetts Institute of Technology
+ *
+ * Permission to use, copy, modify, distribute, and sell this software and its
+ * documentation for any purpose is hereby granted without fee, provided that
+ * the above copyright notice appear in all copies and that both that
+ * copyright notice and this permission notice appear in supporting
+ * documentation, and that the name of M.I.T. not be used in advertising or
+ * publicity pertaining to distribution of the software without specific,
+ * written prior permission.  M.I.T. makes no representations about the
+ * suitability of this software for any purpose.  It is provided "as is"
+ * without express or implied warranty.
+ */
 
 /*LINTLIBRARY*/
 
