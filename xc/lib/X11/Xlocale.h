9ef7862f9260e367aa4f444fe9616485141f7581
rws <empty>
1994-04-12 21:36:16 +0000

    add MIT copyright and permission

--- xc/lib/X11/Xlocale.h
+++ xc/lib/X11/Xlocale.h
@@ -1,4 +1,18 @@
-/* $XConsortium: Xlocale.h,v 1.7 91/04/10 10:23:57 rws Exp $ */
+/* $XConsortium: Xlocale.h,v 1.8 91/04/10 10:41:17 rws Exp $ */
+
+/*
+ * Copyright 1991 Massachusetts Institute of Technology
+ *
+ * Permission to use, copy, modify, distribute, and sell this software and its
+ * documentation for any purpose is hereby granted without fee, provided that
+ * the above copyright notice appear in all copies and that both that
+ * copyright notice and this permission notice appear in supporting
+ * documentation, and that the name of M.I.T. not be used in advertising or
+ * publicity pertaining to distribution of the software without specific,
+ * written prior permission.  M.I.T. makes no representations about the
+ * suitability of this software for any purpose.  It is provided "as is"
+ * without express or implied warranty.
+ */
 
 #ifndef _XLOCALE_H_
 #define _XLOCALE_H_
--- xc/lib/X11/cursorfont.h
+++ xc/lib/X11/cursorfont.h
@@ -1,4 +1,19 @@
-/* $XConsortium: cursorfont.h,v 1.1 87/09/11 07:56:26 toddb Exp $ */
+/* $XConsortium: cursorfont.h,v 1.2 88/09/06 16:44:27 jim Exp $ */
+
+/*
+ * Copyright 1987 Massachusetts Institute of Technology
+ *
+ * Permission to use, copy, modify, distribute, and sell this software and its
+ * documentation for any purpose is hereby granted without fee, provided that
+ * the above copyright notice appear in all copies and that both that
+ * copyright notice and this permission notice appear in supporting
+ * documentation, and that the name of M.I.T. not be used in advertising or
+ * publicity pertaining to distribution of the software without specific,
+ * written prior permission.  M.I.T. makes no representations about the
+ * suitability of this software for any purpose.  It is provided "as is"
+ * without express or implied warranty.
+ */
+
 #define XC_num_glyphs 154
 #define XC_X_cursor 0
 #define XC_arrow 2
