160b506b4b9c91a5de41566b495cd189d8f16a1c
dpw <empty>
1994-04-12 21:59:06 +0000

    copyright/permission notice

--- xc/programs/xfs/include/resource.h
+++ xc/programs/xfs/include/resource.h
@@ -1,17 +1,20 @@
-/* $XConsortium$ */
+/* $XConsortium: resource.h,v 1.2 91/05/13 16:49:21 gildea Exp $ */
 /* 
  * Copyright 1990, 1991 Network Computing Devices; 
  * Portions Copyright 1987 by Digital Equipment Corporation and the 
  * Massachusetts Institute of Technology
  * 
- * Permission to use, copy, modify, and distribute this protoype software 
- * and its documentation to Members and Affiliates of the MIT X Consortium 
- * any purpose and without fee is hereby granted, provided
- * that the above copyright notice appear in all copies and that both that
+ * Permission to use, copy, modify, distribute, and sell this software and its
+ * documentation for any purpose is hereby granted without fee, provided that
+ * the above copyright notice appear in all copies and that both that
  * copyright notice and this permission notice appear in supporting
- * documentation, and that the names of Network Computing Devices, Digital or
- * MIT not be used in advertising or publicity pertaining to distribution of
- * the software without specific, written prior permission.
+ * documentation, and that the names of M.I.T., Network Computing Devices,
+ * or Digital not be used in advertising or
+ * publicity pertaining to distribution of the software without specific,
+ * written prior permission.  M.I.T. Network Computing Devices, or Digital
+ * make no representations about the
+ * suitability of this software for any purpose.  It is provided "as is"
+ * without express or implied warranty.
  * 
  * NETWORK COMPUTING DEVICES, DIGITAL AND MIT DISCLAIM ALL WARRANTIES WITH
  * REGARD TO THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
