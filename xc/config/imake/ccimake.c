0ebd1d991f9ec395f7c148aaaddb429bca9dcf2e
gildea <empty>
1994-04-13 16:42:14 +0000

    fix copyright

--- xc/config/imake/ccimake.c
+++ xc/config/imake/ccimake.c
@@ -1,6 +1,19 @@
 /*
- * $XConsortium$
- * 
+ * $XConsortium: ccimake.c,v 1.12 89/10/16 12:09:23 jim Exp $
+ *
+ * Copyright 1993 Massachusetts Institute of Technology
+ *
+ * Permission to use, copy, modify, distribute, and sell this software and its
+ * documentation for any purpose is hereby granted without fee, provided that
+ * the above copyright notice appear in all copies and that both that
+ * copyright notice and this permission notice appear in supporting
+ * documentation, and that the name of M.I.T. not be used in advertising or
+ * publicity pertaining to distribution of the software without specific,
+ * written prior permission.  M.I.T. makes no representations about the
+ * suitability of this software for any purpose.  It is provided "as is"
+ * without express or implied warranty.
+ */
+/* 
  * Warning:  This file must be kept as simple as posible so that it can 
  * compile without any special flags on all systems.  Do not touch it unless
  * you *really* know what you're doing.  Make changes in imakemdep.h, not here.
--- xc/config/imake/imakemdep.h
+++ xc/config/imake/imakemdep.h
@@ -1,6 +1,19 @@
 /*
- * $XConsortium: imakemdep.h,v 1.73 94/03/29 15:54:01 gildea Exp $
- * 
+ * $XConsortium: imakemdep.h,v 1.74 94/04/01 10:44:11 rws Exp $
+ *
+ * Copyright 1993 Massachusetts Institute of Technology
+ *
+ * Permission to use, copy, modify, distribute, and sell this software and its
+ * documentation for any purpose is hereby granted without fee, provided that
+ * the above copyright notice appear in all copies and that both that
+ * copyright notice and this permission notice appear in supporting
+ * documentation, and that the name of M.I.T. not be used in advertising or
+ * publicity pertaining to distribution of the software without specific,
+ * written prior permission.  M.I.T. makes no representations about the
+ * suitability of this software for any purpose.  It is provided "as is"
+ * without express or implied warranty.
+ */
+/* 
  * This file contains machine-dependent constants for the imake utility.
  * When porting imake, read each of the steps below and add in any necessary
  * definitions.  In general you should *not* edit ccimake.c or imake.c!
