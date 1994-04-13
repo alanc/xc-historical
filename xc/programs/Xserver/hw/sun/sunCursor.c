0100aa11ffe557827498080a75f0b8666135db20
gildea <empty>
1994-04-13 18:20:00 +0000

    add copyright

--- xc/programs/Xserver/hw/sun/sunCursor.c
+++ xc/programs/Xserver/hw/sun/sunCursor.c
@@ -1,4 +1,18 @@
-/* $XConsortium: sunCursor.c,v 5.16 93/10/29 17:40:22 kaleb Exp $ */
+/* $XConsortium: sunCursor.c,v 5.17 94/01/07 09:52:21 dpw Exp $ */
+/* Copyright 1988 Sun Microsystems, Inc. */
+/*
+ * Copyright 1993 Massachusetts Institute of Technology
+ *
+ * Permission to use, copy, modify, distribute, and sell this software and its
+ * documentation for any purpose is hereby granted without fee, provided that
+ * the above copyright notice appear in all copies and that both that
+ * copyright notice and this permission notice appear in supporting
+ * documentation, and that the name of M.I.T. not be used in advertising or
+ * publicity pertaining to distribution of the software without specific,
+ * written prior permission.  M.I.T. makes no representations about the
+ * suitability of this software for any purpose.  It is provided "as is"
+ * without express or implied warranty.
+ */
 /*-
  * sunCursor.c --
  *	Functions for maintaining the Sun software cursor...
