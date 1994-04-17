21fe46f97987eca3394f05e96e231778fdb6ec03
converse <empty>
1994-04-17 20:12:11 +0000

    new X Consortium copyright/permission

--- xc/lib/Xt/VarCreate.c
+++ xc/lib/Xt/VarCreate.c
@@ -1,9 +1,7 @@
-/* $XConsortium: VarCreate.c,v 1.28 93/10/06 17:41:19 kaleb Exp $ */
+/* $XConsortium: VarCreate.c,v 1.29 94/03/30 20:30:56 converse Exp $ */
 
 /*
 
-Copyright 1985, 1986, 1987, 1988, 1989 by the
-Massachusetts Institute of Technology
 Copyright 1993 by Sun Microsystems, Inc. Mountain View, CA.
 
 Permission to use, copy, modify, and distribute this
@@ -11,10 +9,10 @@ software and its documentation for any purpose and without
 fee is hereby granted, provided that the above copyright
 notice appear in all copies and that both that copyright
 notice and this permission notice appear in supporting
-documentation, and that the names M.I.T. or Sun not be used in
+documentation, and that the name Sun not be used in
 advertising or publicity pertaining to distribution of the
 software without specific, written prior permission.
-M.I.T. and Sun makes no representations about the suitability of
+Sun makes no representations about the suitability of
 this software for any purpose.  It is provided "as is"
 without express or implied warranty.
 
@@ -26,6 +24,33 @@ ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE,  DATA  OR
 PROFITS,  WHETHER  IN  AN  ACTION OF CONTRACT, NEGLIGENCE OR
 OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION  WITH
 THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+*/
+
+/*
+
+Copyright (c) 1885, 1986, 1987, 1988, 1989, 1994  X Consortium
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+X CONSORTIUM BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of the X Consortium shall not be
+used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from the X Consortium.
 
 */
 
--- xc/lib/Xt/sharedlib.c
+++ xc/lib/Xt/sharedlib.c
@@ -1,11 +1,31 @@
-/* $XConsortium: sharedlib.c,v 1.16 94/03/30 19:55:04 converse Exp $
- * 
- * Copyright 1989 Massachusetts Institute of Technology
- *
- * This file is used to force shared libraries to get the right routines.  For
- * Sun shared libraries, this only wants to be compiled when we are *not*
- * generating shared code so that this gets copied into the application binary.
- */
+/* $XConsortium: sharedlib.c,v 1.16 94/03/30 20:32:53 converse Exp $ */
+
+/*
+
+Copyright (c) 1989, 1994  X Consortium
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+X CONSORTIUM BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name of the X Consortium shall not be
+used in advertising or otherwise to promote the sale, use or other dealings
+in this Software without prior written authorization from the X Consortium.
+
+*/
 
 #if (defined(SUNSHLIB) || defined(AIXSHLIB)) && !defined(SHAREDCODE)
 #include "IntrinsicI.h"
