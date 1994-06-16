8a94c722f67e0aae0e0214eac30d52f6e26e652a
mor <empty>
1994-06-16 13:17:00 +0000

    proxy must have its own connection with the SM. proxy must save .smproxy database file.

--- xc/programs/smproxy/smproxy.c
+++ xc/programs/smproxy/smproxy.c
@@ -1,4 +1,4 @@
-/* $XConsortium: smproxy.c,v 1.1 94/06/06 11:32:01 mor Exp $ */
+/* $XConsortium: smproxy.c,v 1.3 94/06/15 14:50:00 mor Exp $ */
 /******************************************************************************
 
 Copyright (c) 1994  X Consortium
@@ -52,14 +52,367 @@ typedef struct {
     SmcConn smc_conn;
     XtInputId input_id;
     char *client_id;
+    char **wm_command;
+    int wm_command_count;
+    XClassHint class;
+    char *wm_name;
 } WinInfo;
 
+typedef struct ProxyFileEntry
+{
+    struct ProxyFileEntry *next;
+    char *client_id;
+    XClassHint class;
+    char *wm_name;
+    int wm_command_count;
+    char **wm_command;
+} ProxyFileEntry;
+
+ProxyFileEntry *proxyFileHead = NULL;
+
 Bool debug = 0;
+
+SmcConn proxy_smcConn;
+XtInputId proxy_iceInputId;
 
 WinInfo win_list[100];
 int win_count = 0;
 int proxy_count = 0;
 int die_count = 0;
+
+Bool ok_to_die = 0;
+
+Bool gotFirstSave = 0;
+
+int Argc;
+char **Argv;
+
+
+
+int
+write_byte (file, b)
+
+FILE		*file;
+unsigned char   b;
+
+{
+    if (fwrite ((char *) &b, 1, 1, file) != 1)
+	return 0;
+    return 1;
+}
+
+
+int
+write_short (file, s)
+
+FILE		*file;
+unsigned short	s;
+
+{
+    unsigned char   file_short[2];
+
+    file_short[0] = (s & (unsigned)0xff00) >> 8;
+    file_short[1] = s & 0xff;
+    if (fwrite ((char *) file_short, (int) sizeof (file_short), 1, file) != 1)
+	return 0;
+    return 1;
+}
+
+
+int
+write_counted_string (file, string)
+
+FILE	*file;
+char	*string;
+
+{
+    if (string)
+    {
+	unsigned char count = strlen (string);
+
+	if (write_byte (file, count) == 0)
+	    return 0;
+	if (fwrite (string, (int) sizeof (char), (int) count, file) != count)
+	    return 0;
+    }
+    else
+    {
+	if (write_byte (file, 0) == 0)
+	    return 0;
+    }
+
+    return 1;
+}
+
+
+
+int
+read_byte (file, bp)
+
+FILE		*file;
+unsigned char	*bp;
+
+{
+    if (fread ((char *) bp, 1, 1, file) != 1)
+	return 0;
+    return 1;
+}
+
+
+int
+read_short (file, shortp)
+
+FILE		*file;
+unsigned short	*shortp;
+
+{
+    unsigned char   file_short[2];
+
+    if (fread ((char *) file_short, (int) sizeof (file_short), 1, file) != 1)
+	return 0;
+    *shortp = file_short[0] * 256 + file_short[1];
+    return 1;
+}
+
+
+int
+read_counted_string (file, stringp)
+
+FILE	*file;
+char	**stringp;
+
+{
+    unsigned char  len;
+    char	   *data;
+
+    if (read_byte (file, &len) == 0)
+	return 0;
+    if (len == 0) {
+	data = 0;
+    } else {
+    	data = (char *) malloc ((unsigned) len + 1);
+    	if (!data)
+	    return 0;
+    	if (fread (data, (int) sizeof (char), (int) len, file) != len) {
+	    free (data);
+	    return 0;
+    	}
+	data[len] = '\0';
+    }
+    *stringp = data;
+    return 1;
+}
+
+
+
+/*
+ * An entry in the .smproxy file looks like this:
+ *
+ * FIELD				BYTES
+ * -----                                ----
+ * client ID len			1
+ * client ID				LIST of bytes
+ * WM_CLASS "res name" length		1
+ * WM_CLASS "res name"			LIST of bytes
+ * WM_CLASS "res class" length          1
+ * WM_CLASS "res class"                 LIST of bytes
+ * WM_NAME length			1
+ * WM_NAME				LIST of bytes
+ * WM_COMMAND arg count			1
+ * For each arg in WM_COMMAND
+ *    arg length			1
+ *    arg				LIST of bytes
+ */
+
+int
+WriteProxyFileEntry (proxyFile, theWindow)
+
+FILE *proxyFile;
+WinInfo *theWindow;
+
+{
+    int i;
+
+    if (!write_counted_string (proxyFile, theWindow->client_id))
+	return 0;
+    if (!write_counted_string (proxyFile, theWindow->class.res_name))
+	return 0;
+    if (!write_counted_string (proxyFile, theWindow->class.res_class))
+	return 0;
+    if (!write_counted_string (proxyFile, theWindow->wm_name))
+	return 0;
+    
+    if (!theWindow->wm_command || theWindow->wm_command_count == 0)
+    {
+	if (!write_byte (proxyFile, 0))
+	    return 0;
+    }
+    else
+    {
+	if (!write_byte (proxyFile, (char) theWindow->wm_command_count))
+	    return 0;
+	for (i = 0; i < theWindow->wm_command_count; i++)
+	    if (!write_counted_string (proxyFile, theWindow->wm_command[i]))
+		return 0;
+    }
+
+    return 1;
+}
+
+
+int
+ReadProxyFileEntry (proxyFile, pentry)
+
+FILE *proxyFile;
+ProxyFileEntry **pentry;
+
+{
+    ProxyFileEntry *entry;
+    char byte;
+    int i;
+
+    *pentry = entry = (ProxyFileEntry *) malloc (
+	sizeof (ProxyFileEntry));
+    if (!*pentry)
+	return 0;
+
+    entry->client_id = NULL;
+    entry->class.res_name = NULL;
+    entry->class.res_class = NULL;
+    entry->wm_name = NULL;
+    entry->wm_command = NULL;
+    entry->wm_command_count = 0;
+
+    if (!read_counted_string (proxyFile, &entry->client_id))
+	goto give_up;
+    if (!read_counted_string (proxyFile, &entry->class.res_name))
+	goto give_up;
+    if (!read_counted_string (proxyFile, &entry->class.res_class))
+	goto give_up;
+    if (!read_counted_string (proxyFile, &entry->wm_name))
+	goto give_up;
+    
+    if (!read_byte (proxyFile, &byte))
+	goto give_up;
+    entry->wm_command_count = byte;
+
+    if (entry->wm_command_count == 0)
+	entry->wm_command = NULL;
+    else
+    {
+	entry->wm_command = (char **) malloc (entry->wm_command_count *
+	    sizeof (char *));
+
+	if (!entry->wm_command)
+	    goto give_up;
+
+	for (i = 0; i < entry->wm_command_count; i++)
+	    if (!read_counted_string (proxyFile, &entry->wm_command[i]))
+		goto give_up;
+    }
+
+    return 1;
+
+give_up:
+
+    if (entry->client_id)
+	free (entry->client_id);
+    if (entry->class.res_name)
+	free (entry->class.res_name);
+    if (entry->class.res_class)
+	free (entry->class.res_class);
+    if (entry->wm_name)
+	free (entry->wm_name);
+    if (entry->wm_command_count)
+    {
+	for (i = 0; i < entry->wm_command_count; i++)
+	    if (entry->wm_command[i])
+		free (entry->wm_command[i]);
+    }
+    if (entry->wm_command)
+	free ((char *) entry->wm_command);
+    
+    free ((char *) entry);
+    *pentry = NULL;
+
+    return 0;
+}
+
+
+void
+ReadProxyFile ()
+
+{
+    FILE *proxyFile;
+    char *home, filename[128];
+    ProxyFileEntry *entry;
+    int done = 0;
+
+    home = (char *) getenv ("HOME");
+    if (!home)
+	home = ".";
+
+    sprintf (filename, "%s/.smproxy", home);
+
+    proxyFile = fopen (filename, "rb");
+    if (!proxyFile)
+	return;
+
+    while (!done)
+    {
+	if (ReadProxyFileEntry (proxyFile, &entry))
+	{
+	    entry->next = proxyFileHead;
+	    proxyFileHead = entry;
+	}
+	else
+	    done = 1;
+    }
+
+    fclose (proxyFile);
+}
+
+
+
+char *
+LookupClientID (theWindow)
+
+WinInfo *theWindow;
+
+{
+    ProxyFileEntry *ptr;
+    int found = 0;
+
+    ptr = proxyFileHead;
+    while (ptr && !found)
+    {
+	if (strcmp (theWindow->class.res_name, ptr->class.res_name) == 0 &&
+	    strcmp (theWindow->class.res_class, ptr->class.res_class) == 0 &&
+	    strcmp (theWindow->wm_name, ptr->wm_name) == 0)
+	{
+	    int i;
+
+	    if (theWindow->wm_command_count == ptr->wm_command_count)
+	    {
+		for (i = 0; i < theWindow->wm_command_count; i++)
+		    if (strcmp (theWindow->wm_command[i],
+			ptr->wm_command[i]) != 0)
+			break;
+
+		if (i == theWindow->wm_command_count)
+		    found = 1;
+	    }
+	}
+
+	if (!found)
+	    ptr = ptr->next;
+    }
+
+    if (found)
+	return (ptr->client_id);
+    else
+	return NULL;
+}
 
 
 
@@ -95,21 +448,19 @@ Window window;
 
 
 Bool
-HasWmCommand (window)
+GetWmCommand (window, argv_ret, argc_ret)
 
 Window window;
+char ***argv_ret;
+int *argc_ret;
 
 {
-    char **argv_ret = NULL;
-    int argc_ret;
+    *argv_ret = NULL;
 
-    if (!XGetCommand (disp, window, &argv_ret, &argc_ret))
+    if (!XGetCommand (disp, window, argv_ret, argc_ret))
 	return (0);
 
-    if (argv_ret)
-	XFreeStringList (argv_ret);
-
-    return (argc_ret > 0);
+    return (*argc_ret > 0);
 }
 
 
@@ -155,11 +506,14 @@ void FinishSaveYourself (winInfo)
 WinInfo *winInfo;
 
 {
-    char **argv_ret = NULL;
-    int argc_ret;
+    if (winInfo->wm_command)
+    {
+	XFreeStringList (winInfo->wm_command);
+	winInfo->wm_command = NULL;
+    }
 
-    if (!XGetCommand (disp, winInfo->window,
-	&argv_ret, &argc_ret) || argc_ret == 0)
+    if (!GetWmCommand (winInfo->window,
+	&winInfo->wm_command, &winInfo->wm_command_count))
     {
 	SmcSaveYourselfDone (winInfo->smc_conn, False);
     }
@@ -171,28 +525,28 @@ WinInfo *winInfo;
 
 	prop1.name = SmRestartCommand;
 	prop1.type = SmLISTofARRAY8;
-	prop1.num_vals = argc_ret;
+	prop1.num_vals = winInfo->wm_command_count;
 
 	prop1.vals = (SmPropValue *) malloc (
-	    argc_ret * sizeof (SmPropValue));
+	    winInfo->wm_command_count * sizeof (SmPropValue));
 
-	for (i = 0; i < argc_ret; i++)
+	for (i = 0; i < winInfo->wm_command_count; i++)
 	{
-	    prop1.vals[i].value = (SmPointer) argv_ret[i];
-	    prop1.vals[i].length = strlen (argv_ret[i]) + 1;
+	    prop1.vals[i].value = (SmPointer) winInfo->wm_command[i];
+	    prop1.vals[i].length = strlen (winInfo->wm_command[i]) + 1;
 	}
 
 	prop2.name = SmCloneCommand;
 	prop2.type = SmLISTofARRAY8;
-	prop2.num_vals = argc_ret;
+	prop2.num_vals = winInfo->wm_command_count;
 	prop2.vals = prop1.vals;
 
 	prop3.name = SmProgram;
 	prop3.type = SmARRAY8;
 	prop3.num_vals = 1;
 	prop3.vals = &prop3val;
-	prop3val.value = argv_ret[0];
-	prop3val.length = strlen (argv_ret[0]) + 1;
+	prop3val.value = winInfo->wm_command[0];
+	prop3val.length = strlen (winInfo->wm_command[0]) + 1;
 
 	props[0] = &prop1;
 	props[1] = &prop2;
@@ -201,7 +555,6 @@ WinInfo *winInfo;
 	SmcSetProperties (winInfo->smc_conn, 3, props);
 
 	free (prop1.vals);
-	XFreeStringList (argv_ret);
 
 	SmcSaveYourselfDone (winInfo->smc_conn, True);
     }
@@ -291,17 +644,25 @@ SmPointer clientData;
     if (debug)
 	printf ("Trying to kill 0x%x\n", winInfo->window);
 
+#if 0
     XSync (disp, 0);
     XKillClient (disp, winInfo->window);
     XSync (disp, 0);
+#endif
 
 
-    /* Proxy must exit when all clients die */
+    /*
+     * Proxy must exit when all clients die, and the proxy itself
+     * must have received a Die.
+     */
 
     die_count++;
 
-    if (die_count == proxy_count)
+    if (die_count == proxy_count && ok_to_die)
+    {
+	SmcCloseConnection (proxy_smcConn, 0, NULL);
 	exit();
+    }
 }
 
 
@@ -348,7 +709,7 @@ XtInputId	*id;
 
 
 void
-ConnectToSM (winInfo)
+ConnectClientToSM (winInfo)
 
 WinInfo *winInfo;
 
@@ -357,6 +718,7 @@ WinInfo *winInfo;
     unsigned long mask;
     SmcCallbacks callbacks;
     IceConn ice_conn;
+    char *prevId;
 
     mask = SmcSaveYourselfProcMask | SmcDieProcMask |
 	SmcSaveCompleteProcMask | SmcShutdownCancelledProcMask;
@@ -373,6 +735,8 @@ WinInfo *winInfo;
     callbacks.shutdown_cancelled.callback = ShutdownCancelledCB;
     callbacks.shutdown_cancelled.client_data = (SmPointer) winInfo;
 
+    prevId = LookupClientID (winInfo);
+
     winInfo->smc_conn = SmcOpenConnection (
 	NULL, 			/* use SESSION_MANAGER env */
 	(SmPointer) winInfo,	/* force a new connection */
@@ -380,7 +744,7 @@ WinInfo *winInfo;
 	SmProtoMinor,
 	mask,
 	&callbacks,
-	NULL,			/* previous ID */
+	prevId,
 	&winInfo->client_id,
 	256, errorMsg);
 
@@ -418,6 +782,8 @@ Bool map;
     Window window;
     int i;
 
+    printf ("MAP ID = %x\n", event->window);
+
     for (i = 0; i < win_count; i++)
     {
 	if (win_list[i].frame == event->window ||
@@ -438,7 +804,9 @@ Bool map;
 
 	has_xsmp_support = HasXSMPsupport (window);
 	has_save_yourself = HasSaveYourself (window);
-	has_wm_command = HasWmCommand (window);
+	has_wm_command = GetWmCommand (window,
+	    &win_list[win_count].wm_command,
+	    &win_list[win_count].wm_command_count);
 	has_transient_for = HasTransientFor (window);
 
 	win_list[win_count].frame = frame;
@@ -449,9 +817,19 @@ Bool map;
 
 	if (!has_xsmp_support && !has_transient_for &&
 	    (has_save_yourself || has_wm_command))
-	        ConnectToSM (&win_list[win_count]);
+	{
+	    XFetchName (disp, window, &win_list[win_count].wm_name);
+	    win_list[win_count].class.res_name = NULL;
+	    win_list[win_count].class.res_class = NULL;
+	    XGetClassHint (disp, window, &win_list[win_count].class);
+	    
+	    ConnectClientToSM (&win_list[win_count]);
+	}
 	else
+	{
 	    win_list[win_count].smc_conn = NULL;
+	    win_list[win_count].client_id = NULL;
+	}
 
 	if (debug)
 	{
@@ -477,14 +855,12 @@ HandleDestroy (event)
 XDestroyWindowEvent *event;
 
 {
-    Window frame;
     int i;
-
-    frame = event->window;
 
     for (i = 0; i < win_count; i++)
     {
-	if (win_list[i].frame == frame)
+	if (win_list[i].frame == event->window ||
+	    win_list[i].window == event->window)
 	{
 	    if (win_list[i].smc_conn)
 	    {
@@ -498,6 +874,15 @@ XDestroyWindowEvent *event;
 		    win_list[i].frame, win_list[i].window);
 		printf ("\n");
 	    }
+
+	    if (win_list[i].client_id)
+		free (win_list[i].client_id);
+
+	    if (win_list[i].wm_command)
+		XFreeStringList (win_list[i].wm_command);
+
+	    if (win_list[i].wm_name)
+		XFree (win_list[i].wm_name);
 
 	    if (i < win_count - 1)
 		win_list[i] = win_list[win_count - 1];
@@ -548,6 +933,191 @@ XPropertyEvent *event;
 
 
 
+void
+ProxySaveYourselfCB (smcConn, clientData, saveType,
+    shutdown, interactStyle, fast)
+
+SmcConn smcConn;
+SmPointer clientData;
+int saveType;
+Bool shutdown;
+int interactStyle;
+Bool fast;
+
+{
+    FILE *proxyFile;
+    char *home, filename[128];
+    Bool success = True;
+    int i;
+
+    if (!gotFirstSave)
+    {
+	SmProp prop1, prop2, prop3, *props[3];
+	SmPropValue prop3val;
+	int i;
+
+	prop1.name = SmRestartCommand;
+	prop1.type = SmLISTofARRAY8;
+	prop1.num_vals = Argc;
+
+	prop1.vals = (SmPropValue *) malloc (
+	    Argc * sizeof (SmPropValue));
+
+	for (i = 0; i < Argc; i++)
+	{
+	    prop1.vals[i].value = (SmPointer) Argv[i];
+	    prop1.vals[i].length = strlen (Argv[i]) + 1;
+	}
+
+	prop2.name = SmCloneCommand;
+	prop2.type = SmLISTofARRAY8;
+	prop2.num_vals = Argc;
+	prop2.vals = prop1.vals;
+    
+	prop3.name = SmProgram;
+	prop3.type = SmARRAY8;
+	prop3.num_vals = 1;
+	prop3.vals = &prop3val;
+	prop3val.value = Argv[0];
+	prop3val.length = strlen (Argv[0]) + 1;
+
+	props[0] = &prop1;
+	props[1] = &prop2;
+	props[2] = &prop3;
+
+	SmcSetProperties (smcConn, 3, props);
+
+	free (prop1.vals);
+	
+	SmcSaveYourselfDone (smcConn, True);
+
+	gotFirstSave = 1;
+	return;
+    }
+
+    /* We should really get a phase 2 */
+
+    home = (char *) getenv ("HOME");
+    if (!home)
+	home = ".";
+
+    sprintf (filename, "%s/.smproxy", home);
+    proxyFile = fopen (filename, "wb");
+
+    for (i = 0; i < win_count; i++)
+    {
+	if (win_list[i].client_id)
+	    if (!WriteProxyFileEntry (proxyFile, &win_list[i]))
+		success = False;
+    }
+
+    fclose (proxyFile);
+
+    SmcSaveYourselfDone (smcConn, success);
+}
+
+
+
+void
+ProxyDieCB (smcConn, clientData)
+
+SmcConn smcConn;
+SmPointer clientData;
+
+{
+    XtRemoveInput (proxy_iceInputId);
+    ok_to_die = 1;
+}
+
+
+
+void
+ProxySaveCompleteCB (smcConn, clientData)
+
+SmcConn smcConn;
+SmPointer clientData;
+
+{
+    ;
+}
+
+
+
+void
+ProxyShutdownCancelledCB (smcConn, clientData)
+
+SmcConn smcConn;
+SmPointer clientData;
+
+{
+    ;
+}
+
+
+
+ConnectProxyToSM ()
+
+{
+    char errorMsg[256];
+    unsigned long mask;
+    SmcCallbacks callbacks;
+    IceConn iceConn;
+    char *clientId;
+
+    mask = SmcSaveYourselfProcMask | SmcDieProcMask |
+	SmcSaveCompleteProcMask | SmcShutdownCancelledProcMask;
+
+    callbacks.save_yourself.callback = ProxySaveYourselfCB;
+    callbacks.save_yourself.client_data = (SmPointer) NULL;
+
+    callbacks.die.callback = ProxyDieCB;
+    callbacks.die.client_data = (SmPointer) NULL;
+
+    callbacks.save_complete.callback = ProxySaveCompleteCB;
+    callbacks.save_complete.client_data = (SmPointer) NULL;
+
+    callbacks.shutdown_cancelled.callback = ProxyShutdownCancelledCB;
+    callbacks.shutdown_cancelled.client_data = (SmPointer) NULL;
+
+    proxy_smcConn = SmcOpenConnection (
+	NULL, 			/* use SESSION_MANAGER env */
+	(SmPointer) appContext,
+	SmProtoMajor,
+	SmProtoMinor,
+	mask,
+	&callbacks,
+	NULL,			/* previous ID */
+	&clientId,
+	256, errorMsg);
+
+    if (proxy_smcConn == NULL)
+	return;
+
+    iceConn = SmcGetIceConnection (proxy_smcConn);
+
+    proxy_iceInputId = XtAppAddInput (
+	    appContext,
+	    IceConnectionNumber (iceConn),
+            (XtPointer) XtInputReadMask,
+	    ProcessIceMsgProc,
+	    (XtPointer) iceConn);
+}
+
+
+
+int
+my_error (display, event)
+
+Display *display;
+XErrorEvent *event;
+
+{
+    printf ("protocol error!!!!!!!!!!!!\n");
+    exit (1);
+}
+
+
+
 main (argc, argv)
 
 int argc;
@@ -555,6 +1125,9 @@ char **argv;
 
 {
     int zero = 0;
+
+    Argc = argc;
+    Argv = argv;
 
     if (argc > 1)
     {
@@ -576,6 +1149,13 @@ char **argv;
 	fprintf (stderr, "smproxy: unable to open display\n");
 	exit (1);
     }
+
+#if 0
+    XSetErrorHandler (my_error);
+#endif
+
+    ReadProxyFile ();
+    ConnectProxyToSM ();
 
     root = DefaultRootWindow (disp);
 
@@ -599,7 +1179,10 @@ char **argv;
 	    break;
 
 	case UnmapNotify:
+	    printf ("got unmap\n");
+#if 0
 	    HandleMap (&event.xunmap, 0);
+#endif
 	    break;
 
 	case DestroyNotify:
