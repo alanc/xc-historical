ab09523e38e865b31aff76e024fffd01db4fd823
mor <empty>
1994-07-15 14:14:01 +0000

    more work on session menu.  Added delete option + failsafe option.

--- xc/workInProgress/xsm/choose.c
+++ xc/workInProgress/xsm/choose.c
@@ -1,4 +1,4 @@
-/* $XConsortium: choose.c,v 1.4 94/07/12 14:25:01 mor Exp $ */
+/* $XConsortium: choose.c,v 1.5 94/07/15 10:03:39 mor Exp $ */
 /******************************************************************************
 
 Copyright (c) 1993  X Consortium
@@ -28,6 +28,11 @@ in this Software without prior written authorization from the X Consortium.
 #include "xsm.h"
 #include <sys/types.h>
 #include <dirent.h>
+
+static Pixel save_message_foreground;
+static Pixel save_message_background;
+
+static int delete_phase = 0;
 
 
 int
@@ -103,7 +108,7 @@ String *names;
 
 
 
-void
+static void
 AddSessionNames (count, names)
 
 int count;
@@ -115,9 +120,65 @@ String *names;
 }
 
 
+void
+ChooseSession ()
+
+{
+    Dimension   width, height;
+    Position	x, y;
+
+
+    /*
+     * Add the session names to the list
+     */
+
+    AddSessionNames (sessionNameCount, sessionNames);
+
+
+    /*
+     * Center popup containing choice of sessions
+     */
+    
+    XtRealizeWidget (chooseSessionPopup);
+
+    XtVaGetValues (chooseSessionPopup,
+	XtNwidth, &width,
+	XtNheight, &height,
+	NULL);
+
+    x = (Position)(WidthOfScreen (XtScreen (topLevel)) - width) / 2;
+    y = (Position)(HeightOfScreen (XtScreen (topLevel)) - height) / 3;
+
+    XtVaSetValues (chooseSessionPopup,
+	XtNx, x,
+	XtNy, y,
+	NULL);
+
+    XtVaSetValues (chooseSessionListWidget,
+	XtNlongest, width,
+	NULL);
+
+    XtVaSetValues (chooseSessionLabel,
+	XtNwidth, width,
+	NULL);
+
+    XtVaGetValues (chooseSessionMessageLabel,
+	XtNforeground, &save_message_foreground,
+	XtNbackground, &save_message_background,
+	NULL);
+
+    XtVaSetValues (chooseSessionMessageLabel,
+	XtNwidth, width,
+	XtNforeground, save_message_background,
+	NULL);
+
+    XtPopup (chooseSessionPopup, XtGrabNone);
+}
+
+
 
 static void
-ChooseSessionOkXtProc (w, client_data, callData)
+ChooseSessionLoadXtProc (w, client_data, callData)
 
 Widget		w;
 XtPointer 	client_data;
@@ -125,12 +186,10 @@ XtPointer 	callData;
 
 {
     XawListReturnStruct *current;
-    char title[256];
 
 
     /*
-     * Pop down choice of sessions, realize main window, and
-     * start the specified session.
+     * Pop down choice of sessions and start the specified session.
      */
 
     XtPopdown (chooseSessionPopup);
@@ -146,14 +205,129 @@ XtPointer 	callData;
 
     FreeSessionNames (sessionNameCount, sessionNames);
 
-    sprintf (title, "xsm: %s", session_name);
-    XtVaSetValues (topLevel,
-	XtNtitle, title,		/* session name */
-	NULL);
+    StartSession (session_name,
+	False /* look for .SM-<session name> startup file */);
+}
 
-    XtRealizeWidget (topLevel);
-    
-    StartSession (session_name);
+
+
+static void
+ChooseSessionDeleteXtProc (w, client_data, callData)
+
+Widget		w;
+XtPointer 	client_data;
+XtPointer 	callData;
+
+{
+    XawListReturnStruct *current;
+    int longest;
+    char *name;
+
+    current = XawListShowCurrent (chooseSessionListWidget);
+
+    if (!current)
+	return;
+
+    name = current->string;
+
+    if (!name && *name == '\0')
+    {
+	XtFree ((char *) current);
+	return;
+    }
+
+    delete_phase++;
+
+    if (delete_phase == 1)
+    {
+	XtVaSetValues (chooseSessionMessageLabel,
+	    XtNforeground, save_message_foreground,
+            NULL);
+
+	XBell (XtDisplay (topLevel), 0);
+    }
+    else
+    {
+	char command[256];
+	char *dir;
+	int i, j;
+
+	XtVaSetValues (chooseSessionMessageLabel,
+	    XtNforeground, save_message_background,
+            NULL);
+
+	dir = (char *) getenv ("SM_SAVE_DIR");
+	if (!dir)
+	{
+	    dir = (char *) getenv ("HOME");
+	    if (!dir)
+		dir = ".";
+	}
+
+	sprintf (command, "rm %s/.SM-%s", dir, name);
+
+	if (system (command) != -1)
+	{
+	    for (i = 0; i < sessionNameCount; i++)
+	    {
+		if (strcmp (sessionNames[i], name) == 0)
+		{
+		    XtFree ((char *) sessionNames[i]);
+		    for (j = i; j < sessionNameCount - 1; j++)
+			sessionNames[j] = sessionNames[j + 1];
+		    sessionNameCount--;
+		    break;
+		}
+	    }
+
+	    if (sessionNameCount == 0)
+	    {
+		XtSetSensitive (chooseSessionLoadButton, 0);
+		XtSetSensitive (chooseSessionDeleteButton, 0);
+		XtUnmanageChild (chooseSessionListWidget);
+	    }
+	    else
+	    {
+		XtVaGetValues (chooseSessionListWidget,
+		    XtNlongest, &longest,
+		    NULL);
+
+		XawListChange (chooseSessionListWidget,
+		    sessionNames, sessionNameCount, longest, True);
+	    }
+	}
+
+	delete_phase = 0;
+    }
+
+    XtFree ((char *) current);
+}
+
+
+
+static void
+ChooseSessionFailSafeXtProc (w, client_data, callData)
+
+Widget		w;
+XtPointer 	client_data;
+XtPointer 	callData;
+
+{
+    /*
+     * Pop down choice of sessions, and start the fail safe session.
+     */
+
+    XtPopdown (chooseSessionPopup);
+
+    if (session_name)
+	XtFree (session_name);
+
+    session_name = XtNewString ("Fail Safe");
+
+    FreeSessionNames (sessionNameCount, sessionNames);
+
+    StartSession (session_name,
+	True /* Use ~/.xsmstartup if found, else system.xsm */);
 }
 
 
@@ -166,7 +340,16 @@ XtPointer 	client_data;
 XtPointer 	callData;
 
 {
-    exit (0);
+    if (delete_phase > 0)
+    {
+	XtVaSetValues (chooseSessionMessageLabel,
+	    XtNforeground, save_message_background,
+            NULL);
+
+	delete_phase = 0;
+    }
+    else
+	exit (0);
 }
 
 
@@ -195,6 +378,8 @@ create_choose_session_popup ()
         XtNfromHoriz, NULL,
         XtNfromVert, NULL,
         XtNborderWidth, 0,
+	XtNresizable, True,
+	XtNjustify, XtJustifyCenter,
 	NULL);
 
     chooseSessionListWidget = XtVaCreateManagedWidget (
@@ -207,22 +392,47 @@ create_choose_session_popup ()
 	XtNvertDistance, 25,
 	NULL);
 
-    chooseSessionOkButton = XtVaCreateManagedWidget (
-	"chooseSessionOkButton", commandWidgetClass, chooseSessionForm,
+    chooseSessionMessageLabel = XtVaCreateManagedWidget (
+	"chooseSessionMessageLabel", labelWidgetClass, chooseSessionForm,
         XtNfromHoriz, NULL,
         XtNfromVert, chooseSessionListWidget,
-	XtNvertDistance, 25,
+        XtNborderWidth, 0,
+	XtNresizable, True,
+	XtNjustify, XtJustifyCenter,
+	NULL);
+
+    chooseSessionLoadButton = XtVaCreateManagedWidget (
+	"chooseSessionLoadButton", commandWidgetClass, chooseSessionForm,
+        XtNfromHoriz, NULL,
+        XtNfromVert, chooseSessionMessageLabel,
         NULL);
 
-    XtAddCallback (chooseSessionOkButton, XtNcallback,
-	ChooseSessionOkXtProc, 0);
+    XtAddCallback (chooseSessionLoadButton, XtNcallback,
+	ChooseSessionLoadXtProc, 0);
+
+    chooseSessionDeleteButton = XtVaCreateManagedWidget (
+	"chooseSessionDeleteButton", commandWidgetClass, chooseSessionForm,
+        XtNfromHoriz, chooseSessionLoadButton,
+        XtNfromVert, chooseSessionMessageLabel,
+        NULL);
+
+    XtAddCallback (chooseSessionDeleteButton, XtNcallback,
+	ChooseSessionDeleteXtProc, 0);
+
+    chooseSessionFailSafeButton = XtVaCreateManagedWidget (
+	"chooseSessionFailSafeButton", commandWidgetClass, chooseSessionForm,
+        XtNfromHoriz, chooseSessionDeleteButton,
+        XtNfromVert, chooseSessionMessageLabel,
+        NULL);
+
+    XtAddCallback (chooseSessionFailSafeButton, XtNcallback,
+	ChooseSessionFailSafeXtProc, 0);
 
 
     chooseSessionCancelButton = XtVaCreateManagedWidget (
 	"chooseSessionCancelButton", commandWidgetClass, chooseSessionForm,
-        XtNfromHoriz, chooseSessionOkButton,
-        XtNfromVert, chooseSessionListWidget,
-	XtNvertDistance, 25,
+        XtNfromHoriz, chooseSessionFailSafeButton,
+        XtNfromVert, chooseSessionMessageLabel,
         NULL);
 
     XtAddCallback (chooseSessionCancelButton, XtNcallback,
