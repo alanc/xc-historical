2ee131a7281dafe92b9b9c83a5c2975ebfbf184b
kaleb <empty>
1994-10-12 19:40:57 +0000

    3.1

--- xc/programs/Xserver/hw/xfree86/xf86Version.h
+++ xc/programs/Xserver/hw/xfree86/xf86Version.h
@@ -1,3 +1,4 @@
-/* $XConsortium$ */
+/* $XConsortium: xf86Version.h,v 1.1 94/10/05 13:20:29 kaleb Exp $ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/xf86Version.h,v 3.41 1994/09/27 10:27:20 dawes Exp $ */
 
-#define XF86_VERSION " 3.0 "
+#define XF86_VERSION " 3.1 "
