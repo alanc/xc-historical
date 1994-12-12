a77f563cc17b7151c012198d638d3d34bf0d37e1
mor <empty>
1994-12-12 20:01:02 +0000

    remove ExecuteOldDiscardCommands, add DeleteSession

--- xc/workInProgress/xsm/saveutil.h
+++ xc/workInProgress/xsm/saveutil.h
@@ -1,4 +1,4 @@
-/* $XConsortium: saveutil.h,v 1.1 94/07/08 14:06:57 mor Exp $ */
+/* $XConsortium: saveutil.h,v 1.2 94/07/18 15:06:18 mor Exp mor $ */
 /******************************************************************************
 
 Copyright (c) 1993  X Consortium
@@ -27,4 +27,4 @@ in this Software without prior written authorization from the X Consortium.
 
 extern int ReadSave ();
 extern void WriteSave ();
-extern void ExecuteOldDiscardCommands ();
+extern Status DeleteSession ();
