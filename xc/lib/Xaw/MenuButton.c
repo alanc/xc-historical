ed2ae6c00d3d7dd3283f50bea95a9b17fffc2b56
kaleb <empty>
1995-06-26 20:35:12 +0000

    XBUG 7483

--- xc/lib/Xaw/MenuButton.c
+++ xc/lib/Xaw/MenuButton.c
@@ -1,4 +1,4 @@
-/* $XConsortium: MenuButton.c,v 1.19 94/01/31 10:48:27 kaleb Exp $ */
+/* $XConsortium: MenuButton.c,v 1.20 94/04/17 20:12:19 kaleb Exp kaleb $ */
 
 /*
 Copyright (c) 1989, 1994  X Consortium
@@ -59,9 +59,9 @@ static void PopupMenu();
 #define superclass ((CommandWidgetClass)&commandClassRec)
 
 static char defaultTranslations[] = 
-    "<EnterWindow>:     highlight()             \n\
-     <LeaveWindow>:     reset()                 \n\
-     <BtnDown>:         reset() PopupMenu()     ";
+"<EnterWindow>: highlight()\n\
+ <LeaveWindow>: reset()\n\
+ Any<BtnDown>:  reset() PopupMenu()";
 
 /****************************************************************
  *
